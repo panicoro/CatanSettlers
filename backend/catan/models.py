b8b5618de691e404298dabeeaf4cdc4aa894c2b8
Pablo Rosa <bluchisnico@gmail.com>
2019-10-04 18:55:30 -0300

    Agregando Modelo Room

--- backend/catan/models.py
+++ backend/catan/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    max_players = models.IntegerField(default=3,
+                                      validators=[MinValueValidator(3),
+                                                  MaxValueValidator(4)])
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    players = models.ManyToManyField(User, related_name='room_players',
+                                     blank=True)
