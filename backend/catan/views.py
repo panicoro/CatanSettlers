37f8647179a87634630ec2f592fdfa3ee5f982f7
Pablo Rosa <bluchisnico@gmail.com>
2019-10-05 18:43:15 -0300

    Agregando vistas

--- backend/catan/views.py
+++ backend/catan/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from catan.models import Room
+from catan.serializers import RoomSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class RoomList(APIView):
+    def get(self, request, format=None):
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        print(request.data)
+        return Response(serializer.data)
+
+
+class RoomDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise Http404
+
+    def put(self, request, pk, format=None):
+        room = self.get_object(pk)
+        serializer = RoomSerializer(room, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
