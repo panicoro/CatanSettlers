cf0d6ee386b9b85d1085443420f1b325607a5f0d
Pablo Rosa <bluchisnico@gmail.com>
2019-10-05 00:31:46 -0300

    Correción de RoomSerializer y borrado de UserSerializer

--- backend/catan/serializer.py
+++ backend/catan/serializer.py
@@ -3,16 +3,9 @@ from django.contrib.auth.models import User
 from catan.models import Room
 
 
-class UserSerializer(serializers.ModelSerializer):
-    rooms = serializers.PrimaryKeyRelatedField(many=True,
-                                               queryset=Room.objects.all())
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'rooms']
-
-
 class RoomSerializer(serializers.ModelSerializer):
+    players = serializers.StringRelatedField(many=True)
+    owner = serializers.StringRelatedField()                                              
     class Meta:
         model = Room
         fields = ['id', 'name', 'max_players', 'owner', 'players']
