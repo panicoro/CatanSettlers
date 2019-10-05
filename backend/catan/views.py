9d2d26c9886e97328a5f1659be87736492a1af99
Naigaribay <nairagaribay@gmail.com>
2019-10-05 17:27:40 -0300

    ResourceList en views

--- backend/catan/views.py
+++ backend/catan/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Resource
+from .serializers import ResourceSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class ResourceList(generics.ListCreateAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+
+    def get_object(self) :
+        queryset = self.queryset()
+        obj = get_object_or_404(
+            queryset,
+            pk=self.kwargs['pk']
+        )
+
+        return obj
+
+
