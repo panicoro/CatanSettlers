7d1bcc996bdbec44c9d71b5911a5bc272f927221
Veroo <verito.cba933@gmail.com>
2019-10-05 21:45:57 -0300

    Implementacion de las vista lista_tablero, lista_hexagono

--- backend/catan/views.py
+++ backend/catan/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view  #p/ trabajar las vistas basadas en funciones
+from rest_framework.response import Response
+from catan.models import Tablero, Hexagono  #importo mis modelos
+from catan.serializers import TableroSerializer, HexagonoSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def lista_tablero(request, format=None):
+
+    if request.method == 'GET':
+	    tableros = Tablero.objects.all()		#obtengo todos los tableros
+	    serializer = TableroSerializer(tableros, many=True)
+	    return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = TableroSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST'])
+def lista_hexagono(request, format=None):
+
+    if request.method == 'GET':
+	    hexagonos = Hexagono.objects.all()		#obtengo todos los hexagonos
+	    serializer = HexagonoSerializer(hexagonos, many=True)
+	    return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = HexagonoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
