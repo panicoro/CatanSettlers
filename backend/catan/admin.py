c3c06ff2d0cedf3d29fe17bed0882f78e11e6b91
Veroo <verito.cba933@gmail.com>
2019-10-05 21:31:39 -0300

    Incorporacion de los modelos en el archivo admin.py

--- backend/catan/admin.py
+++ backend/catan/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class TableroAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+
+class HexagonoAdmin(admin.ModelAdmin):
+    list_display = ('resource', 'token', 'tablero')
+
+class VerticeAdmin(admin.ModelAdmin):
+    list_display = ('nivel', 'indice', 'tablero')
+
+admin.site.register(Tablero, TableroAdmin)
+admin.site.register(Hexagono, HexagonoAdmin)
+admin.site.register(Vertice, VerticeAdmin)
