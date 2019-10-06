525abbff5ecafe0a75f09ccc536fbe574598b116
Veroo <verito.cba933@gmail.com>
2019-10-05 21:11:45 -0300

    Implementacion de Modelos: Tablero, Hehagono, Vertice

--- backend/catan/models.py
+++ backend/catan/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Tablero(models.Model):
+    nombre = models.CharField(max_length=25, blank=True, default='')
+
+    class Meta:
+        unique_together = ['id', 'nombre']
+        ordering = ['id']
+
+class Hexagono(models.Model):
+    position = models.IntegerField(default=0)
+    resource  = models.CharField(max_length=25)
+    token = models.IntegerField(default= 0)
+    tablero = models.ForeignKey(Tablero, related_name='hexagonos', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['tablero', 'token']
+        ordering = ['token']
+
+    def __str__(self):
+        return '{resource: %s , token: %d}' % (self.resource, self.token)
+
+class Vertice(models.Model):
+    nivel = models.IntegerField(default= 0)
+    indice = models.IntegerField(default= 0)
+    tablero = models.ForeignKey(Tablero, related_name='vertices', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['nivel', 'indice']
+        ordering = ['nivel']
+
+    def __str__(self):
+        return '{nivel: %d , indice: %d}' % (self.nivel, self.indice)
