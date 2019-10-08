78f4a50410a09fe8d46b3ef7a4673dd9fb2903b8
Marco Weihmuller <fedemarkco@gmail.com>
2019-10-07 23:36:30 -0300

    Se corrige URL users/login/ en el login.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,8 +20,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/login/', TokenObtainPairView.as_view(), name='obtenerToken'),
-    path('user/login/refresh/', TokenRefreshView.as_view(), name='refrescarToken'),
+    path('users/login/', TokenObtainPairView.as_view(), name='obtenerToken'),
+    path('users/login/refresh/', TokenRefreshView.as_view(), name='refrescarToken'),
     #path('hello/', views.HelloView.as_view(), name='hello'),
     #path('user/login/', TokenObtainPairView.as_view())
     #path('api/token/refresh/', TokenRefreshView.as_view())
