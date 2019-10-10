e21511706b909eabf368109006126fc92b244837
Pablo Rosa <bluchisnico@gmail.com>
2019-10-09 22:21:19 -0300

    Agregando métodos de validación de RoomSerializer

--- backend/catan/serializers.py
+++ backend/catan/serializers.py
@@ -25,17 +25,56 @@ class RoomSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'max_players', 'owner', 'players']
 
     def update(self, instance, validated_data):
-        owner_data = validated_data.pop('owner')
-        username = owner_data.pop('username')
-        owner = get_user_model().objects.get_or_create(
-            username=username)[0]
-        instance.owner = owner
+        # Only update the players list...
+        self.instance.players.clear()
         players = validated_data.pop('players')
         for player_data in players:
             username = player_data.pop('username')
-            player = get_user_model().objects.get_or_create(
-                username=username)[0]
+            player = User.objects.filter(username=username)[0]
             instance.players.add(player)
-        instance.max_players = validated_data['max_players']
-        instance.name = validated_data['name']
         return instance
+
+    def validate_owner(self, value):
+        # Owner of the room must be the same
+        instance = self.instance
+        if instance.owner.username != value['username']:
+            raise serializers.ValidationError("Cannot change owner")
+        return value
+
+    def validate_max_players(self, value):
+        # Maximum number of players must be the same
+        if self.instance.max_players != value:
+            raise serializers.ValidationError("Cannot change max players")
+        return value
+
+    def validate_name(self, value):
+        # Name must be the same
+        if self.instance.name != value:
+            raise serializers.ValidationError("Cannot change name")
+        return value
+
+    def validate_players(self, players):
+        # Check repeated player dictionaries
+        seen = set()
+        for player in players:
+            tup_player = tuple(player.items())
+            if tup_player in seen:
+                raise serializers.ValidationError("Players has repeat dicts")
+            else:
+                seen.add(tup_player)
+        # Check if players are registered users
+        for player in players:
+            if not User.objects.filter(username=player['username']).exists():
+                raise serializers.ValidationError(
+                    "Cannot add a non register user")
+        # Check if number to put are allowed
+        if len(players) > (self.instance.max_players - 1):
+            raise serializers.ValidationError("Too many players to add")
+        return players
+
+    def validate(self, data):
+        # Check if owner is in players list
+        if data['owner'] in data['players']:
+            raise serializers.ValidationError(
+                "Cannot add the owner to the players")
+        return data
