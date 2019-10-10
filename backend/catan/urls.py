967867f13cec7b9745f37cac560b9042db79a1db
Pablo Rosa <bluchisnico@gmail.com>
2019-10-09 22:22:53 -0300

    Agregando nombres de vistas

--- backend/catan/urls.py
+++ backend/catan/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from catan import views
 
 urlpatterns = [
-    path('rooms/', views.RoomList.as_view()),
-    path('rooms/<int:pk>/', views.RoomDetail.as_view()),
+    path('rooms/', views.RoomList.as_view(), name='list_rooms'),
+    path('rooms/<int:pk>/', views.RoomDetail.as_view(), name='join_room'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
