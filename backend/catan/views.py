3cc46b7fc1a922d13f9a55b49742b1a876776e36
Pablo Rosa <bluchisnico@gmail.com>
2019-10-10 03:15:15 -0300

    Correción en import de permisos

--- backend/catan/views.py
+++ backend/catan/views.py
@@ -4,7 +4,7 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .permissions import OwnRoomPermission
+from catan.permissions import OwnRoomPermission
 
 
 class RoomList(APIView):
