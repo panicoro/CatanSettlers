d5d92b207fcecd27eedac9446af46d5685555094
Marco Weihmuller <fedemarkco@gmail.com>
2019-10-14 19:45:07 -0300

    Corregido serializers.py en login.

--- backend/catan/serializers.py
+++ backend/catan/serializers.py
@@ -7,14 +7,16 @@ from rest_framework.reverse import reverse as api_reverse
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.utils.six import text_type
 
+
 class SignupSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super(SignupSerializer, cls).get_token(user)
         return token
-        
+
     def validate(self, attrs):
-        user = authenticate(username=attrs.username, password=attrs.password)
+        user = authenticate(username=attrs['username'],
+                            password=attrs['password'])
         data = {}
         data['username'] = user.username
         data['email'] = user.email
@@ -23,5 +25,3 @@ class SignupSerializer(TokenObtainPairSerializer):
         data['refresh'] = text_type(refresh)
         data['access'] = text_type(refresh.access_token)
         return data
-
-
