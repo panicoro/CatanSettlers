5e16c0eff11448c89cbcb8a3a0504732a14aa2b3
Marco Weihmuller <fedemarkco@gmail.com>
2019-10-13 21:29:55 -0300

    Corregido views.py del login.

--- backend/catan/views.py
+++ backend/catan/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt import authentication
 
-# Create your views here.
+class AuthAPIView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        response={"detail":"Invalid credentials"}
+
+        try:
+            data['username'] = data['user']
+            data['password'] = data['pass']
+        except:
+            return Response(response, status=status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            user = authenticate(username=data['username'], password=data['password'])
+        except:
+            user = None
+
+        if (user != None):
+            serializer = self.get_serializer(data=request.data)
+            response = serializer.validate(request.data)
+        else:
+            return Response(response, status=status.HTTP_401_UNAUTHORIZED)
+
+        return Response(response, status=status.HTTP_201_CREATED)
