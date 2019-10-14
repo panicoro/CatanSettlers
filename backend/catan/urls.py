ae2613fc87372ffddffbf744896daec3964fa090
Marco Weihmuller <fedemarkco@gmail.com>
2019-10-14 19:46:50 -0300

    Corregido urls.py en login.

--- backend/catan/urls.py
+++ backend/catan/urls.py
@@ -4,8 +4,10 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('users/login/', views.AuthAPIView.as_view(), name='token_obtain_pair'),
-    path('users/login/refresh/', TokenRefreshView.as_view(), name='refrescarToken'),
+    path('users/login/', views.AuthAPIView.as_view(),
+         name='tokenObtainPair'),
+    path('users/login/refresh/', TokenRefreshView.as_view(),
+         name='refreshToken'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
