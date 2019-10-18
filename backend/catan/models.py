e2e46317334feaae0b704f259311940d6a026e2a
Veroo <verito.cba933@gmail.com>
2019-10-18 13:22:38 -0300

    Modificacion de los Modelos Game, Hexes, VertexPosition

--- backend/catan/models.py
+++ backend/catan/models.py
@@ -10,70 +10,47 @@ class Board(models.Model):
 
 class Game(models.Model):
     name = models.CharField(max_length=25)
-    in_turn = models.CharField(max_length=25)
     board = models.ForeignKey(Board, related_name='games', on_delete=models.CASCADE)
+
     class Meta:
         unique_together = ['id', 'name']
         ordering = ['id']
 
-    def __str__(self):
-        return '{name: %s , in_turn: %s}' % (self.name, self.in_turn)
+RESOURCE_TYPE = [
+    ('BRICK','brick'),
+    ('LUMBER','lumber'),
+    ('WOOL','wool'),
+    ('GRAIN','grain'),
+    ('ORE','ore'),
+]
 
 class Hexes(models.Model):
-
-    POSITION_AVAILABLES = [
-            ('1','(0, 0)'),    
-            ('2','(1, 0)'),
-            ('3','(1, 1)'),
-            ('4','(1, 2)'),
-            ('5','(1, 3)'),
-            ('6','(1, 4)'), 
-            ('7','(1, 5)'),
-            ('8','(2, 0)'),
-            ('9','(2, 1)'),
-            ('10','(2, 2)'),
-            ('11','(2, 3)'),
-            ('12','(2, 4)'),
-            ('13','(2, 5)'),
-            ('14','(2, 6)'),
-            ('15','(2, 7)'),
-            ('16','(2, 8)'),
-            ('17','(2, 9)'),
-            ('18','(2, 10'),
-            ('19','(2, 11'),
-    ]
-
-    position = models.CharField(max_length=2, choices=POSITION_AVAILABLES)
-    resource  = models.CharField(max_length=25)
-    token = models.IntegerField(default= 0)
+    TERRAIN_TYPE = [('DESERT', 'desert')] + RESOURCE_TYPE
+    terrain = models.CharField(max_length= 6, choices=TERRAIN_TYPE)
+    token = models.IntegerField(default= 0, validators=[MinValueValidator(2),
+                                                        MaxValueValidator(12)])
     board = models.ForeignKey(Board, related_name='hexes', on_delete=models.CASCADE)
 
-    class Meta:
-        unique_together = ['board', 'position']
-        ordering = ['token']
-
-    def __str__(self):
-        return '{position:{%s} ,resource: %s , token: %d}' % (self.position, self.resource, self.token)
-
-class Vertex(models.Model):
-    level = models.IntegerField(default= 0)
-    index = models.IntegerField(default= 0)
-    board = models.ForeignKey(Board, related_name='vertex', on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ['level', 'index']
-        ordering = ['level']
-
-    def __str__(self):
-        return '{level: %d , index: %d}' % (self.level, self.index)
 
 class VertexPosition(models.Model):
-    level = models.IntegerField(default= 0)
-    index = models.IntegerField(default= 0)
+    level = models.IntegerField(default= 0, validators=[MinValueValidator(0),
+                                                        MaxValueValidator(2)])
+    index = models.IntegerField(default= 0, validators=[MinValueValidator(0),
+                                                        MaxValueValidator(11)])
+    position = models.OneToOneField(Hexes, related_name='position', on_delete=models.CASCADE, null=True)
     
     class Meta:
         unique_together = ['level', 'index']
         ordering = ['level']
+
+    def clean(self):
+        if (self.level == 0) and not (0 <= self.index<= 0 ):
+            raise ValidationError('The index with level 0 must be between 0 and 0.')
+        if (self.level == 1) and not (0 <= self.index <= 5):
+            raise ValidationError('The index with level 1 must be between 0 and 5.')
+        if (self.level == 2) and not (0 <= self.index <= 11):
+            raise ValidationError('The index with level 2 must be between 0 and 11.')
+
 
     def __str__(self):
         return '{level: %d , index: %d}' % (self.level, self.index)
