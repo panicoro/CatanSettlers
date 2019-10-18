af613d666eec7da72cf51d1c4aa62a235b4fef07
Naigaribay <nairagaribay@gmail.com>
2019-10-18 15:21:06 -0300

    Cambiando modelos Game, Player, Resource con pycodestyle pasado.

--- backend/catan/models.py
+++ backend/catan/models.py
@@ -1,22 +1,46 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Game(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=25)
+    in_turn = models.ForeignKey(User, related_name='in_turn',
+                                on_delete=models.CASCADE, null=True)
+    winner = models.ForeignKey(User, on_delete=models.CASCADE,
+                               blank=True, null=True)
+    roober = models.IntegerField()
+
+    class Meta:
+        unique_together = ['id', 'name']
+        ordering = ['id']
+
 
 class Player(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
-    game = models.ForeignKey(Game,related_name='players',on_delete = models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    colour = models.CharField(max_length=50)
+    development_cards = models.IntegerField(default=0,
+                                            validators=[MinValueValidator(0)])
+    resources_cards = models.IntegerField(default=0,
+                                          validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return '%s' % (self.username)
+
 
 class Resource(models.Model):
-
     RESOURCE_TYPE = [
-        ('BRICK','brick'),
-        ('LUMBER','lumber'),
-        ('WOOL','wool'),
-        ('GRAIN','grain'),
-        ('ORE','ore')
+        ('BRICK', 'brick'),
+        ('LUMBER', 'lumber'),
+        ('WOOL', 'wool'),
+        ('GRAIN', 'grain'),
+        ('ORE', 'ore')
     ]
-    name = models.CharField(max_length= 6, choices=RESOURCE_TYPE)
+
     owner = models.ForeignKey(Player, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    resource_name = models.CharField(max_length=6, choices=RESOURCE_TYPE)
+
+    def __str__(self):
+        return self.resource_name
