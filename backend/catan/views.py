fc5cf1b37668fcf69e446cb098d367d13c6af121
Naigaribay <nairagaribay@gmail.com>
2019-10-19 18:15:06 -0300

    Cambiando resource en PlayerInfo

--- backend/catan/views.py
+++ backend/catan/views.py
@@ -1,15 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from .models import Game, Resource, Player
-from .serializers import GameSerializer, ResourceSerializer, PlayerSerializer
+from catan.models import *
+from catan.serializers import *
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 
-class ResourceList(APIView):
 
-    def get(self, request, pk):        
+class PlayerInfo(APIView):
+    def get(self, request, pk):
+        resource_list = []
+        game = get_object_or_404(Game, pk=pk)
         user = self.request.user
-        player_id = Player.objects.filter(username=user).get().id
-        queryset = Resource.objects.filter(owner=player_id)
-        serializer = ResourceSerializer(queryset, many=True)
-        return Response(serializer.data)
+        player_id = Player.objects.filter(username=user, game=pk).get().id
+        queryset_cards = Card.objects.filter(owner=player_id)
+        queryset_resource = Resource.objects.filter(owner=player_id)
+        serializer_cards = CardSerializer(queryset_cards, many=True)
+        serializer_resource = ResourceSerializer(queryset_resource, many=True)
+        for resource in serializer_resource.data:
+            print(resource.values())
+            resource_list.append(resource.values())
+            print(resource_list)
+
+        data = {'resources': resource_list,
+                'cards': serializer_cards.data}
+        return Response(data)
