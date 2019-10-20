099288f6ea508c85e62725edbf976e051c5d1b73
Pablo Rosa <bluchisnico@gmail.com>
2019-10-20 02:35:55 -0300

    Corrigiendo views

--- backend/catan/views.py
+++ backend/catan/views.py
@@ -1,18 +1,31 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from catan.models import Board, Game
-from catan.serializers import GameListSerializer, BoardSerializer
+from catan.models import Board, Game, Hexe
+from catan.serializers import (
+                                GameListSerializer,
+                                BoardSerializer,
+                                HexeSerializer
+                              )
+
 
 class GameList(APIView):
-    def get(self, requests, format=None):
+    def get(self, request, format=None):
         games = Game.objects.all()
-        gamesserializers = GameListSerializer(games, many=True)
-        return Response(gamesserializers.data)
+        games_serializers = GameListSerializer(games, many=True)
+        return Response(games_serializers.data)
+
+
+class BoardList(APIView):
+    def get(self, request, format=None):
+        boards = Board.objects.all()
+        boards_serializers = BoardSerializer(boards, many=True)
+        return Response(boards_serializers.data)
+
 
 class BoardInfo(APIView):
     def get(self, request, pk):
-        board = get_object_or_404(Board, pk=pk)
-        board = Board.objects.get(pk=pk)
-        serializer_board = BoardSerializer(board)
-        return Response(serializer_board.data)
+        game = get_object_or_404(Game, pk=pk)
+        board_hexes = Hexe.objects.filter(board=game.board.id)
+        hexes_serializer = HexeSerializer(board_hexes, many=True)
+        return Response({"hexes": hexes_serializer.data})
