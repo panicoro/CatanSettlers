3228d2d0535acf9e8cfbfc6ff0d5f7de17b631c8
Pablo Rosa <bluchisnico@gmail.com>
2019-10-20 02:33:17 -0300

    Agregando url para boards

--- backend/catan/urls.py
+++ backend/catan/urls.py
@@ -4,6 +4,7 @@ from catan import views
 
 urlpatterns = [
     path('games/', views.GameList.as_view()),
+    path('boards/', views.BoardList.as_view()),
     path('games/<int:pk>/board/', views.BoardInfo.as_view(),
          name='BoardInfo'),
 ]
