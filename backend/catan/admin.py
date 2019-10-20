fa19a08acc2274ae3d7f6aba4cdc5558c61e7c14
Pablo Rosa <bluchisnico@gmail.com>
2019-10-20 02:43:00 -0300

    Agregando nuevo modelo a admin.py

--- backend/catan/admin.py
+++ backend/catan/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from catan.models import VertexPosition, Hexes, Board, Game
+from catan.models import HexePosition, Hexe, Board, Game
 
-admin.site.register(VertexPosition)
-admin.site.register(Hexes)
+admin.site.register(HexePosition)
+admin.site.register(Hexe)
 admin.site.register(Board)
 admin.site.register(Game)
