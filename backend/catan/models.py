b2ac7a5a86f18ba60e29aa350b26f724051c1fd0
Veroo <verito.cba933@gmail.com>
2019-10-20 17:43:16 -0300

    Agregado de libreria

--- backend/catan/models.py
+++ backend/catan/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.exceptions import ValidationError
 
 
 class Board(models.Model):
