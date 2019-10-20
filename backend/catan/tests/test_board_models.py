597198632cdd472221aea2e5ede00d1e49d8683b
Veroo <verito.cba933@gmail.com>
2019-10-20 17:44:49 -0300

    Cambios en el archivo test_board_models.py

--- backend/catan/tests/test_board_models.py
+++ backend/catan/tests/test_board_models.py
@@ -1,5 +1,6 @@
 import pytest
-from catan.models import Board, Hexe, HexePosition,Game
+from catan.models import Board, Hexe, HexePosition, Game
+from django.core.exceptions import ValidationError
 
 
 @pytest.mark.django_db
@@ -22,14 +23,16 @@ class TestModels:
     def test_hexe(self):
         board = Board.objects.create(name='Colonos')
         hexe_position = HexePosition.objects.create(level=1, index=1)
-        hexe = Hexe.objects.create(terrain='ORE', token=2, board=board, position=hexe_position)
+        hexe = Hexe.objects.create(terrain='ORE', token=2, board=board,
+                                   position=hexe_position)
         assert hexe.board == board
         assert hexe.position == hexe_position
 
     def test_of_all(self):
         board = Board.objects.create(name='Colonos')
         hexe_position = HexePosition.objects.create(level=1, index=1)
-        hexe = Hexe.objects.create(terrain='ORE', token=2, board=board, position=hexe_position)
+        hexe = Hexe.objects.create(terrain='ORE', token=2, board=board,
+                                   position=hexe_position)
         assert board.name == 'Colonos'
         assert hexe_position.level == hexe.position.level
         assert hexe_position.index == hexe.position.index
@@ -38,38 +41,24 @@ class TestModels:
         assert hexe.token == 2
         assert hexe.board == board
 
-    def test_of_all_falla_uno(self):
-        board = Board.objects.create(name='Colonos')
+    def test_HexePositionNoValid(self):
         hexe_position = HexePosition.objects.create(level=0, index=1)
-        hexe = Hexe.objects.create(terrain='ORE', token=3, board=board, position=hexe_position)
-        assert board.name == 'Colonos'
-        assert hexe_position.level == hexe.position.level
-        assert hexe_position.index == hexe.position.index
-        assert hexe.position == hexe_position
-        assert hexe.terrain == 'ORE'
-        assert hexe.token == 3
-        assert hexe.board == board
+        try:
+            hexe_position.full_clean()
+        except ValidationError as e:
+            error = 'The index with level 0 must be between 0 and 0.'
+            assert error in e.message_dict['__all__']
 
-    def test_of_all_falla_dos(self):
-        board = Board.objects.create(name='Colonos')
-        hexe_position = HexePosition.objects.create(level=1, index=7)
-        hexe = Hexe.objects.create(terrain='ORE', token=4, board=board, position=hexe_position)
-        assert board.name == 'Colonos'
-        assert hexe_position.level == hexe.position.level
-        assert hexe_position.index == hexe.position.index
-        assert hexe.position == hexe_position
-        assert hexe.terrain == 'ORE'
-        assert hexe.token == 4
-        assert hexe.board == board
+        hexe_position = HexePosition.objects.create(level=1, index=6)
+        try:
+            hexe_position.full_clean()
+        except ValidationError as e:
+            error = 'The index with level 1 must be between 0 and 5.'
+            assert error in e.message_dict['__all__']
 
-    def test_of_all_falla_tres(self):
-        board = Board.objects.create(name='Colonos')
-        hexe_position = HexePosition.objects.create(level=2, index=13)
-        hexe = Hexe.objects.create(terrain='ORE', token=5, board=board, position=hexe_position)
-        assert board.name == 'Colonos'
-        assert hexe_position.level == hexe.position.level
-        assert hexe_position.index == hexe.position.index
-        assert hexe.position == hexe_position
-        assert hexe.terrain == 'ORE'
-        assert hexe.token == 5
-        assert hexe.board == board
+        hexe_position = HexePosition.objects.create(level=2, index=12)
+        try:
+            hexe_position.full_clean()
+        except ValidationError as e:
+            error = 'The index with level 2 must be between 0 and 11.'
+            assert error in e.message_dict['__all__']
