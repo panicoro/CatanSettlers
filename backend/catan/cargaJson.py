156d421d460d284c92c607896136d3926ee31182
Naigaribay <nairagaribay@gmail.com>
2019-10-23 16:42:53 -0300

    Agregando funcion ResourceBuild en cargaJon

--- backend/catan/cargaJson.py
+++ backend/catan/cargaJson.py
@@ -1,7 +1,8 @@
 import json
 
+
 def Hexagono(level,index):
-    with open('HexaVerVecinos.json') as file:
+    with open('catan/HexaVerVecinos.json') as file:
         data = json.load(file)
 
         for aux in data['data']:
@@ -11,14 +12,32 @@ def Hexagono(level,index):
 
 
 def Vertice(level, index):
-    with open('VertexVecinos.json') as file:
+    with open('catan/VertexVecinos.json') as file:
         data = json.load(file)
 
-        for aux in data["hexagonosVecinos"]:
+        for aux in data["data"]:
             vertice = aux['vertice']
             if level== vertice[0] and index== vertice[1]:
                 return aux['vecinos']
 
+def ResourceBuild(list_resource):
+    ladrillo = True
+    madera = True
+    lana = True 
+    cereal = True
+    rta = []
+    for resource in list_resource:
+        if resource.resource_name =="brick" and ladrillo:
+            ladrillo = False
+            rta.append(resource)
+        if resource.resource_name == "lumber" and madera:
+            madera = False
+            rta.append(resource)
+        if resource.resource_name == "wool" and lana:
+            lana = False
+            rta.append(resource)
+        if resource.resource_name == "grain" and cereal:
+            cereal = False
+            rta.append(resource)
 
-
-    
+    return rta
