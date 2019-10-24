1b1644d5e446cb699fb7b52a69cd130e625c2395
Naigaribay <nairagaribay@gmail.com>
2019-10-23 23:21:39 -0300

    Agregando test de url BuildSettlement

---
+++ backend/catan/tests/test_BuildSettlement_url.py
@@ -0,0 +1,8 @@
+import pytest
+from django.urls import reverse, resolve
+
+
+class TestUrls:
+    def test_BuildSettlement_url(self):
+        path = reverse('BuildSettlement', kwargs={'pk': 1})
+        assert resolve(path).view_name == 'BuildSettlement'
