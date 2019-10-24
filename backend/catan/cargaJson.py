2860f9fd1a3c50eaa1968eb900cff5249a770bbb
Naigaribay <nairagaribay@gmail.com>
2019-10-23 21:49:50 -0300

    Pasando pycodestyle en cargaJson

--- backend/catan/cargaJson.py
+++ backend/catan/cargaJson.py
@@ -1,13 +1,13 @@
 import json
 
 
-def HexagonInfo(level,index):
+def HexagonInfo(level, index):
     with open('catan/HexaVerVecinos.json') as file:
         data = json.load(file)
 
         for aux in data['data']:
-            hexagon=aux['hexagono']
-            if level== hexagon[0] and index== hexagon[1]:
+            hexagon = aux['hexagono']
+            if level == hexagon[0] and index == hexagon[1]:
                 return aux['vecinos']
 
 
@@ -17,17 +17,18 @@ def VertexInfo(level, index):
 
         for aux in data["data"]:
             vertex = aux['vertice']
-            if level== vertex[0] and index== vertex[1]:
+            if level == vertex[0] and index == vertex[1]:
                 return aux['vecinos']
+
 
 def ResourceBuild(list_resource):
     birck = True
     lumber = True
-    wool = True 
+    wool = True
     grain = True
     rta = []
     for resource in list_resource:
-        if resource.resource_name =="brick" and birck:
+        if resource.resource_name == "brick" and birck:
             birck = False
             rta.append(resource)
         if resource.resource_name == "lumber" and lumber:
@@ -56,21 +57,22 @@ def CheckRoad(list_road, level, index):
                 return rta
     return rta
 
+
 def CheckBuild(list_build, list_vertex, level, index):
     rta = True
     for build in list_build:
         for vertex in list_vertex:
             if build.position.level == vertex[0]:
                 if build.position.index == vertex[1]:
-                    rta = False    
+                    rta = False
                     return rta
-            if  build.position.level == level:
+            if build.position.level == level:
                 if build.position.index == index:
                     rta = False
                     return rta
     return rta
 
+
 def deleteResource(list_resource):
     for resource in list_resource:
-            resource.delete()
-
+        resource.delete()
