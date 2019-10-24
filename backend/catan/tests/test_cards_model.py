e3b9ee9bdbf1d9bfb1b04267f891d41459625c0f
Pablo Rosa <bluchisnico@gmail.com>
2019-10-24 00:33:41 -0300

    Agregando test

--- backend/catan/tests/test_cards_model.py
+++ backend/catan/tests/test_cards_model.py
@@ -27,9 +27,7 @@ class TestModels:
 
     def test__str__(self):
         hexe_position = HexePosition.objects.create(level=1, index=2)
-
         board = Board.objects.create(name='Colonos')
-
         game = Game.objects.create(name='Juego', board=board,
                                    robber=hexe_position)
         user1 = mixer.blend(User, username='Nico', password='minombrenico')
@@ -42,3 +40,81 @@ class TestModels:
         assert str(card) == card.card_name
         resource = mixer.blend(Resource, resource_name='wool', owner=player1)
         assert str(resource) == resource.resource_name
+
+    def test_Not_valid_Card(self):
+        hexe_position = HexePosition.objects.create(level=1, index=2)
+        board = Board.objects.create(name='Colonos')
+        game1 = Game.objects.create(name='Juego', board=board,
+                                    robber=hexe_position)
+        game2 = Game.objects.create(name='Juego', board=board,
+                                    robber=hexe_position)
+        user1 = mixer.blend(User, username='Nico', password='minombrenico')
+        player1 = mixer.blend(Player, username=user1,
+                              game=game1, colour='Rojo',
+                              development_cards=1, resources_cards=2)
+        card = mixer.blend(Card, owner=player1,
+                           game=game2, card_name='monopoly')
+        try:
+            card.full_clean()
+        except ValidationError as e:
+            error = 'Cannot be player of other game'
+            assert error in e.message_dict['__all__']
+
+    def test_Not_valid_Resource(self):
+        hexe_position = HexePosition.objects.create(level=1, index=2)
+        board = Board.objects.create(name='Colonos')
+        game1 = Game.objects.create(name='Juego', board=board,
+                                    robber=hexe_position)
+        game2 = Game.objects.create(name='Juego', board=board,
+                                    robber=hexe_position)
+        user1 = mixer.blend(User, username='Nico', password='minombrenico')
+        player1 = mixer.blend(Player, username=user1,
+                              game=game1, colour='Rojo',
+                              development_cards=1, resources_cards=2)
+        resource = mixer.blend(Resource, owner=player1,
+                               game=game2, resource_name='wool')
+        try:
+            resource.full_clean()
+        except ValidationError as e:
+            error = 'Cannot be player of other game'
+            assert error in e.message_dict['__all__']
+
+    def test_Not_valid_Building(self):
+        hexe_position = HexePosition.objects.create(level=1, index=2)
+        board = Board.objects.create(name='Colonos')
+        game1 = Game.objects.create(name='Juego', board=board,
+                                    robber=hexe_position)
+        game2 = Game.objects.create(name='Juego', board=board,
+                                    robber=hexe_position)
+        user1 = mixer.blend(User, username='Nico', password='minombrenico')
+        player1 = mixer.blend(Player, username=user1,
+                              game=game1, colour='Rojo',
+                              development_cards=1, resources_cards=2)
+        building = mixer.blend(Building, owner=player1,
+                               game=game2, name='city')
+        try:
+            building.full_clean()
+        except ValidationError as e:
+            error = 'Cannot be player of other game'
+            assert error in e.message_dict['__all__']
+
+    def test_Not_valid_Road(self):
+        hexe_position = HexePosition.objects.create(level=1, index=2)
+        board = Board.objects.create(name='Colonos')
+        game1 = Game.objects.create(name='Juego', board=board,
+                                    robber=hexe_position)
+        game2 = Game.objects.create(name='Juego', board=board,
+                                    robber=hexe_position)
+        user1 = mixer.blend(User, username='Nico', password='minombrenico')
+        player1 = mixer.blend(Player, username=user1,
+                              game=game1, colour='Rojo',
+                              development_cards=1, resources_cards=2)
+        vertex1 = VertexPosition.objects.create(level=1, index=2)
+        vertex2 = VertexPosition.objects.create(level=1, index=1)
+        road = mixer.blend(Road, owner=player1,
+                           game=game2, vertex_1=vertex1, vertex_2=vertex2)
+        try:
+            road.full_clean()
+        except ValidationError as e:
+            error = 'Cannot be player of other game'
+            assert error in e.message_dict['__all__']
---
+++ backend/catan/tests/test_game_view.py
@@ -0,0 +1,66 @@
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from catan.models import *
+from catan.views import GameList, GameInfo
+from rest_framework.test import force_authenticate
+from rest_framework_simplejwt.tokens import AccessToken
+import pytest
+import json
+
+
+@pytest.mark.django_db
+class TestView(TestCase):
+
+    def setUp(self):
+        self.username = 'test_user'
+        self.email = 'test_user@example.com'
+        self.user = User.objects.create_user(self.username, self.email)
+        self.token = AccessToken()
+
+    def test_GameList(self):
+        path = reverse('Games')
+        request = RequestFactory().get(path)
+        force_authenticate(request, user=self.user, token=self.token)
+        view = GameList.as_view()
+        response = view(request)
+        assert response.status_code == 200
+
+    def test_GameInfoNoExists(self):
+        path = reverse('GameInfo', kwargs={'pk': 1})
+        request = RequestFactory().get(path)
+        force_authenticate(request, user=self.user, token=self.token)
+        view = GameInfo.as_view()
+        response = view(request, pk=1)
+        response.render()
+        assert response.status_code == 404
+
+    def test_GameInfo(self):
+        hexe_position = HexePosition.objects.create(level=1, index=2)
+        board = Board.objects.create(name='Colonos')
+        game1 = Game.objects.create(name='Juego', board=board,
+                                    robber=hexe_position)
+        user1 = mixer.blend(User, username='Nico', password='minombrenico')
+        user2 = mixer.blend(User, username='Pablo', password='minombrepablo')
+        player1 = mixer.blend(Player, username=user1,
+                              game=game1, colour='yellow',
+                              development_cards=1, resources_cards=2)
+        player2 = mixer.blend(Player, username=user2,
+                              game=game1, colour='green',
+                              development_cards=1, resources_cards=2)
+        current_turn = mixer.blend(Current_Turn, game=game1, user=user1)
+        vertex1 = VertexPosition.objects.create(level=1, index=2)
+        vertex2 = VertexPosition.objects.create(level=1, index=1)
+        resource1 = mixer.blend(Resource, owner=player1,
+                                game=game1, resource_name='wool',
+                                last_gained=True)
+        road = mixer.blend(Road, owner=player1,
+                           game=game1, vertex_1=vertex1, vertex_2=vertex2)
+        path = reverse('GameInfo', kwargs={'pk': 1})
+        request = RequestFactory().get(path)
+        force_authenticate(request, user=self.user, token=self.token)
+        view = GameInfo.as_view()
+        response = view(request, pk=1)
+        response.render()
+        assert response.status_code == 200
