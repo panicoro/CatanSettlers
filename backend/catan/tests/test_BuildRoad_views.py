dffac1b0314119135686a3df6cd76cc034fb666e
Veroo <verito.cba933@gmail.com>
2019-11-01 09:33:07 -0300

    Refactorizacion de varios test, en el archivo test_BuildRoad.views.py

--- backend/catan/tests/test_BuildRoad_views.py
+++ backend/catan/tests/test_BuildRoad_views.py
@@ -1,6 +1,6 @@
 import pytest
 from django.contrib.auth.models import User
-from django.test import RequestFactory
+from django.test import TestCase, RequestFactory
 from catan.models import *
 from catan.views import BuildRoad
 from django.urls import reverse
@@ -11,136 +11,113 @@ from catan.cargaJson import *
 
 
 @pytest.mark.django_db
-class TestViews:
+class TestViews(TestCase):
 
-    def test_build_road_case1_403(self):
+    def setUp(self):
+        self.username = 'test_user'
+        self.email = 'test_user@example.com'
+        self.user = User.objects.create_user(self.username, self.email)
         self.token = AccessToken()
-        path = reverse('BuildRoad', kwargs={'pk': 1})
-        vert_position1 = VertexPosition.objects.create(level=1, index=18)
-        vert_position2 = VertexPosition.objects.create(level=2, index=1)
-        hexe_position = HexePosition.objects.create(level=0, index=0)
-        board = Board.objects.create(name='Colonos')
+        self.vert_position1 = VertexPosition.objects.create(level=2, index=0)
+        self.vert_position2 = VertexPosition.objects.create(level=2, index=1)
+        self.hexe_position = HexePosition.objects.create(level=0, index=0)
+        self.board = Board.objects.create(name='Colonos')
+        self.game = Game.objects.create(id=1, name='Juego1', board=self.board,
+                                        robber=self.hexe_position,
+                                        winner=self.user)
+        self.player = Player.objects.create(turn=1, username=self.user,
+                                            colour='YELLOW', game=self.game,
+                                            development_cards=0,
+                                            resources_cards=2,
+                                            victory_points=0)
 
+    def test_not_neighbor(self):
+        path = reverse('BuildRoad', kwargs={'pk': 1})
+        vert_position1 = VertexPosition.objects.create(level=2, index=18)
+        vert_position2 = VertexPosition.objects.create(level=2, index=20)
         data = {"type": "build_road",
-                "payload": {"level1": "1", "index1": "18",
-                            "level2": "2", "index2": "1"}}
+                "payload": [{"level": 2, "index": 18},
+                            {"level": 2, "index": 20}]}
         request = RequestFactory().post(path, data,
                                         content_type='application/json')
-        request.user = User.objects.create(username='nvero',
-                                           password='bariloche')
-        force_authenticate(request, user=request.user, token=self.token)
-        game = Game.objects.create(id=1, name='Juego1', board=board,
-                                   robber=hexe_position, winner=request.user)
-        player = Player.objects.create(turn=1, username=request.user,
-                                       colour='YELLOW', game=game,
-                                       development_cards=0, resources_cards=2,
-                                       victory_points=0)
-
+        force_authenticate(request, user=self.user, token=self.token)
         view = BuildRoad.as_view()
         response = view(request, pk=1)
+        assert response.data == {'detail': 'not neighbor'}
         assert response.status_code == 403
 
-    def test_build_road_case2_403(self):
-        self.token = AccessToken()
+    def test_not_resource(self):
         path = reverse('BuildRoad', kwargs={'pk': 1})
-        vert_position1 = VertexPosition.objects.create(level=1, index=18)
-        vert_position2 = VertexPosition.objects.create(level=2, index=1)
-        hexe_position = HexePosition.objects.create(level=0, index=0)
-        board = Board.objects.create(name='Colonos')
-
         data = {"type": "build_road",
-                "payload": {"level1": "1", "index1": "18",
-                            "level2": "2", "index2": "1"}}
+                "payload": [{"level": 2, "index": 0},
+                            {"level": 2, "index": 1}]}
         request = RequestFactory().post(path, data,
                                         content_type='application/json')
-        request.user = User.objects.create(username='nvero',
-                                           password='bariloche')
-        force_authenticate(request, user=request.user, token=self.token)
-        game = Game.objects.create(id=1, name='Juego1', board=board,
-                                   robber=hexe_position, winner=request.user)
-        player = Player.objects.create(turn=1, username=request.user,
-                                       colour='YELLOW', game=game,
-                                       development_cards=0, resources_cards=2,
-                                       victory_points=0)
+        force_authenticate(request, user=self.user, token=self.token)
+        view = BuildRoad.as_view()
+        response = view(request, pk=1)
+        assert response.data == {'detail': "Doesn't have enough resources"}
+        assert response.status_code == 403
 
-        Resource.objects.create(owner=player, game=game, resource_name='brick')
-        Resource.objects.create(owner=player, game=game,
+    def test_nothing_built(self):
+        path = reverse('BuildRoad', kwargs={'pk': 1})
+        data = {"type": "build_road",
+                "payload": [{"level": 2, "index": 0},
+                            {"level": 2, "index": 1}]}
+        request = RequestFactory().post(path, data,
+                                        content_type='application/json')
+        force_authenticate(request, user=self.user, token=self.token)
+        Resource.objects.create(owner=self.player, game=self.game,
+                                resource_name='brick')
+        Resource.objects.create(owner=self.player, game=self.game,
                                 resource_name='lumber')
         view = BuildRoad.as_view()
         response = view(request, pk=1)
+        assert response.data == {'detail': 'must have something built'}
         assert response.status_code == 403
 
-    def test_build_road_case3_403(self):
-        self.token = AccessToken()
+    def test_invalid_position(self):
         path = reverse('BuildRoad', kwargs={'pk': 1})
-        vert_position1 = VertexPosition.objects.create(level=1, index=18)
-        vert_position2 = VertexPosition.objects.create(level=2, index=1)
-        hexe_position = HexePosition.objects.create(level=0, index=0)
-        board = Board.objects.create(name='Colonos')
-
         data = {"type": "build_road",
-                "payload": {"level1": "1", "index1": "18",
-                            "level2": "2", "index2": "1"}}
+                "payload": [{"level": 2, "index": 0},
+                            {"level": 2, "index": 1}]}
         request = RequestFactory().post(path, data,
                                         content_type='application/json')
-        request.user = User.objects.create(username='nvero',
-                                           password='bariloche')
-        force_authenticate(request, user=request.user, token=self.token)
-        game = Game.objects.create(id=1, name='Juego1', board=board,
-                                   robber=hexe_position, winner=request.user)
-        player = Player.objects.create(turn=1, username=request.user,
-                                       colour='YELLOW', game=game,
-                                       development_cards=0, resources_cards=2,
-                                       victory_points=0)
-
-        Resource.objects.create(owner=player, game=game, resource_name='brick')
-        Resource.objects.create(owner=player, game=game,
+        force_authenticate(request, user=self.user, token=self.token)
+        Resource.objects.create(owner=self.player, game=self.game,
+                                resource_name='brick')
+        Resource.objects.create(owner=self.player, game=self.game,
                                 resource_name='lumber')
-        Road.objects.create(owner=player, vertex_1=vert_position1,
-                            vertex_2=vert_position2, game=game)
-
+        Road.objects.create(owner=self.player, vertex_1=self.vert_position1,
+                            vertex_2=self.vert_position2, game=self.game)
         view = BuildRoad.as_view()
         response = view(request, pk=1)
+        assert response.data == {'detail': 'invalid position, reserved'}
         assert response.status_code == 403
 
-    def test_build_road_200(self):
+    def test_build_road(self):
         self.token = AccessToken()
         path = reverse('BuildRoad', kwargs={'pk': 1})
-        vert_position1 = VertexPosition.objects.create(level=1, index=18)
-        vert_position2 = VertexPosition.objects.create(level=2, index=1)
-
-        vp1 = VertexPosition.objects.create(level=2, index=0)
-        vp2 = VertexPosition.objects.create(level=2, index=29)
-
-        hexe_position = HexePosition.objects.create(level=0, index=0)
-        board = Board.objects.create(name='Colonos')
-
         data = {"type": "build_road",
-                "payload": {"level1": "1", "index1": "18",
-                            "level2": "2", "index2": "1"}}
+                "payload": [{"level": 2, "index": 0},
+                            {"level": 2, "index": 1}]}
         request = RequestFactory().post(path, data,
                                         content_type='application/json')
-
         request.user = User.objects.create(username='nvero',
                                            password='bariloche')
-        force_authenticate(request, user=request.user, token=self.token)
-        game = Game.objects.create(id=1, name='Juego1', board=board,
-                                   robber=hexe_position, winner=request.user)
-        player = Player.objects.create(turn=1, username=request.user,
-                                       colour='YELLOW', game=game,
-                                       development_cards=0, resources_cards=2,
-                                       victory_points=0)
-
-        Resource.objects.create(owner=player, game=game, resource_name='brick')
-        Resource.objects.create(owner=player, game=game,
+        force_authenticate(request, user=self.user, token=self.token)
+        Resource.objects.create(owner=self.player, game=self.game,
+                                resource_name='brick')
+        Resource.objects.create(owner=self.player, game=self.game,
                                 resource_name='lumber')
-
-        road = Road.objects.create(owner=player, vertex_1=vp1, vertex_2=vp2,
-                                   game=game)
-        building = Building.objects.create(game=game, name='settlement',
-                                           owner=player,
-                                           position=vert_position1)
-
+        vp2 = VertexPosition.objects.create(level=2, index=2)
+        road = Road.objects.create(owner=self.player,
+                                   vertex_1=self.vert_position2,
+                                   vertex_2=vp2,
+                                   game=self.game)
+        building = Building.objects.create(game=self.game, name='settlement',
+                                           owner=self.player,
+                                           position=self.vert_position1)
         view = BuildRoad.as_view()
         response = view(request, pk=1)
         assert response.status_code == 200
