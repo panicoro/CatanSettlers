251b5cd4a4c5594498cfc337049cdf397b331c6b
Naigaribay <nairagaribay@gmail.com>
2019-11-01 12:16:24 -0300

    Agregando test_noTurn y modularizando

--- backend/catan/tests/test_BuildSettlement_url.py
+++ backend/catan/tests/test_BuildSettlement_url.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse, resolve
+
+
 class TestUrls:
     def test_BuildSettlement_url(self):
-        path = reverse('BuildSettlement', kwargs={'pk': 1})
-        assert resolve(path).view_name == 'BuildSettlement'
+        path = reverse('PlayerActions', kwargs={'pk': 1})
+        assert resolve(path).view_name == 'PlayerActions'
--- backend/catan/tests/test_BuildSettlement_views.py
+++ backend/catan/tests/test_BuildSettlement_views.py
@@ -32,95 +32,87 @@ class TestViews(TestCase):
                                             development_cards=1,
                                             resources_cards=4,
                                             victory_points=0)
+        self.turn = Current_Turn.objects.create(game=self.game,
+                                                user=self.user,
+                                                dices1=3,
+                                                dices2=3)
+        self.road = Road.objects.create(owner=self.player, vertex_1=self.vertex_1,
+                                   vertex_2=self.vertex_2, game=self.game)
+        self.brick = Resource.objects.create(owner=self.player, game=self.game,
+                                        resource_name="brick")
+        self.lumber = Resource.objects.create(owner=self.player, game=self.game,
+                                         resource_name="lumber")
+        self.wool = Resource.objects.create(owner=self.player, game=self.game,
+                                       resource_name="wool")
+        self.grain = Resource.objects.create(owner=self.player, game=self.game,
+                                        resource_name="grain")
 
-    def test_build_vertex1(self):
-        path = reverse('BuildSettlement', kwargs={'pk': 1})
+    def test_noTurn(self):
+        user = User.objects.create_user(username='catan', email='matilde13')
+        self.turn.user= user
+        self.turn.save()
+        path = reverse('PlayerActions', kwargs={'pk': 1})
         data = {"type": "build_settlement",
                 "payload": {"level": 1, "index": 16}}
         request = RequestFactory().post(path, data,
                                         content_type='application/json')
         force_authenticate(request, user=self.user, token=self.token)
-        road = Road.objects.create(owner=self.player, vertex_1=self.vertex_1,
-                                   vertex_2=self.vertex_2, game=self.game)
-        brick = Resource.objects.create(owner=self.player, game=self.game,
-                                        resource_name="brick")
-        lumber = Resource.objects.create(owner=self.player, game=self.game,
-                                         resource_name="lumber")
-        wool = Resource.objects.create(owner=self.player, game=self.game,
-                                       resource_name="wool")
-        grain = Resource.objects.create(owner=self.player, game=self.game,
-                                        resource_name="grain")
-        view = BuildSettlement.as_view()
+        view = PlayerActions.as_view()
+        response = view(request, pk=1)
+        response.render()
+        assert response.status_code == 403
+
+    def test_build_vertex1(self):
+        path = reverse('PlayerActions', kwargs={'pk': 1})
+        data = {"type": "build_settlement",
+                "payload": {"level": 1, "index": 16}}
+        request = RequestFactory().post(path, data,
+                                        content_type='application/json')
+        force_authenticate(request, user=self.user, token=self.token)
+        view = PlayerActions.as_view()
         response = view(request, pk=1)
         response.render()
         assert response.status_code == 200
 
     def test_build_vertex2(self):
-        path = reverse('BuildSettlement', kwargs={'pk': 1})
+        path = reverse('PlayerActions', kwargs={'pk': 1})
         data = {"type": "build_settlement",
                 "payload": {"level": 2, "index": 26}}
         request = RequestFactory().post(path, data,
                                         content_type='application/json')
         force_authenticate(request, user=self.user, token=self.token)
-        road = Road.objects.create(owner=self.player, vertex_1=self.vertex_1,
-                                   vertex_2=self.vertex_2, game=self.game)
-        brick = Resource.objects.create(owner=self.player, game=self.game,
-                                        resource_name="brick")
-        lumber = Resource.objects.create(owner=self.player, game=self.game,
-                                         resource_name="lumber")
-        wool = Resource.objects.create(owner=self.player, game=self.game,
-                                       resource_name="wool")
-        grain = Resource.objects.create(owner=self.player, game=self.game,
-                                        resource_name="grain")
-        view = BuildSettlement.as_view()
+        view = PlayerActions.as_view()
         response = view(request, pk=1)
         assert response.status_code == 200
 
     def test_invalidPosition_road(self):
-        path = reverse('BuildSettlement', kwargs={'pk': 1})
+        path = reverse('PlayerActions', kwargs={'pk': 1})
         data = {"type": "build_settlement",
                 "payload": {"level": 2, "index": 26}}
         request = RequestFactory().post(path, data,
                                         content_type='application/json')
         force_authenticate(request, user=self.user, token=self.token)
-        brick = Resource.objects.create(owner=self.player, game=self.game,
-                                        resource_name="brick")
-        lumber = Resource.objects.create(owner=self.player, game=self.game,
-                                         resource_name="lumber")
-        wool = Resource.objects.create(owner=self.player, game=self.game,
-                                       resource_name="wool")
-        grain = Resource.objects.create(owner=self.player, game=self.game,
-                                        resource_name="grain")
-        view = BuildSettlement.as_view()
+        self.road.delete()
+        view = PlayerActions.as_view()
         response = view(request, pk=1)
         assert response.status_code == 403
 
     def test_invalidPosition_build(self):
-        path = reverse('BuildSettlement', kwargs={'pk': 1})
+        path = reverse('PlayerActions', kwargs={'pk': 1})
         data = {"type": "build_settlement",
                 "payload": {"level": 2, "index": 26}}
         request = RequestFactory().post(path, data,
                                         content_type='application/json')
         force_authenticate(request, user=self.user, token=self.token)
-        road = Road.objects.create(owner=self.player, vertex_1=self.vertex_1,
-                                   vertex_2=self.vertex_2, game=self.game)
         build = Building.objects.create(game=self.game, name='city',
                                         owner=self.player,
                                         position=self.vertex_1)
-        brick = Resource.objects.create(owner=self.player, game=self.game,
-                                        resource_name="brick")
-        lumber = Resource.objects.create(owner=self.player, game=self.game,
-                                         resource_name="lumber")
-        wool = Resource.objects.create(owner=self.player, game=self.game,
-                                       resource_name="wool")
-        grain = Resource.objects.create(owner=self.player, game=self.game,
-                                        resource_name="grain")
-        view = BuildSettlement.as_view()
+        view = PlayerActions.as_view()
         response = view(request, pk=1)
         assert response.status_code == 403
 
     def test_busyPosition(self):
-        path = reverse('BuildSettlement', kwargs={'pk': 1})
+        path = reverse('PlayerActions', kwargs={'pk': 1})
         data = {"type": "build_settlement",
                 "payload": {"level": 2, "index": 26}}
         request = RequestFactory().post(path, data,
@@ -129,35 +121,18 @@ class TestViews(TestCase):
         build = Building.objects.create(game=self.game, name='city',
                                         owner=self.player,
                                         position=self.vertex_2)
-        road = Road.objects.create(owner=self.player, vertex_1=self.vertex_1,
-                                   vertex_2=self.vertex_2, game=self.game)
-        brick = Resource.objects.create(owner=self.player, game=self.game,
-                                        resource_name="brick")
-        lumber = Resource.objects.create(owner=self.player, game=self.game,
-                                         resource_name="lumber")
-        wool = Resource.objects.create(owner=self.player, game=self.game,
-                                       resource_name="wool")
-        grain = Resource.objects.create(owner=self.player, game=self.game,
-                                        resource_name="grain")
-        view = BuildSettlement.as_view()
+        view = PlayerActions.as_view()
         response = view(request, pk=1)
         assert response.status_code == 403
 
     def test_noResource(self):
-        path = reverse('BuildSettlement', kwargs={'pk': 1})
+        path = reverse('PlayerActions', kwargs={'pk': 1})
         data = {"type": "build_settlement",
                 "payload": {"level": 2, "index": 26}}
         request = RequestFactory().post(path, data,
                                         content_type='application/json')
         force_authenticate(request, user=self.user, token=self.token)
-        road = Road.objects.create(owner=self.player, vertex_1=self.vertex_1,
-                                   vertex_2=self.vertex_2, game=self.game)
-        brick = Resource.objects.create(owner=self.player, game=self.game,
-                                        resource_name="brick")
-        lumber = Resource.objects.create(owner=self.player, game=self.game,
-                                         resource_name="lumber")
-        wool = Resource.objects.create(owner=self.player, game=self.game,
-                                       resource_name="wool")
-        view = BuildSettlement.as_view()
+        self.grain.delete()
+        view = PlayerActions.as_view()
         response = view(request, pk=1)
         assert response.status_code == 403
