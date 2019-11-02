eec0261d524c5c68877df0585d21f6c9a6f881f9
Veroo <verito.cba933@gmail.com>
2019-11-02 13:17:16 -0300

    Implementacion de test not_turn, not_exist_vertex, y mejoras en los demas test,para el chequeo de las creaciones de los objectos

--- backend/catan/tests/test_BuildRoad_views.py
+++ backend/catan/tests/test_BuildRoad_views.py
@@ -2,7 +2,7 @@ import pytest
 from django.contrib.auth.models import User
 from django.test import TestCase, RequestFactory
 from catan.models import *
-from catan.views import BuildRoad
+from catan.views import *
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import force_authenticate
@@ -20,6 +20,7 @@ class TestViews(TestCase):
         self.token = AccessToken()
         self.vert_position1 = VertexPosition.objects.create(level=2, index=0)
         self.vert_position2 = VertexPosition.objects.create(level=2, index=1)
+        self.vp2 = VertexPosition.objects.create(level=2, index=2)
         self.hexe_position = HexePosition.objects.create(level=0, index=0)
         self.board = Board.objects.create(name='Colonos')
         self.game = Game.objects.create(id=1, name='Juego1', board=self.board,
@@ -28,8 +29,51 @@ class TestViews(TestCase):
         self.player = Player.objects.create(turn=1, username=self.user,
                                             colour='YELLOW', game=self.game,
                                             development_cards=0,
-                                            resources_cards=2,
+                                            resources_cards=0,
                                             victory_points=0)
+        self.turn = Current_Turn.objects.create(game=self.game,
+                                                user=self.user,
+                                                dices1=3,
+                                                dices2=3)
+        self.brick = Resource.objects.create(owner=self.player,
+                                             game=self.game,
+                                             resource_name="brick")
+        self.lumber = Resource.objects.create(owner=self.player,
+                                              game=self.game,
+                                              resource_name="lumber")
+        self.road = Road.objects.create(owner=self.player,
+                                        vertex_1=self.vert_position1,
+                                        vertex_2=self.vert_position2,
+                                        game=self.game)
+
+    def test_not_turn(self):
+        user = User.objects.create_user(username='nvero', email='bariloche')
+        self.turn.user = user
+        self.turn.save()
+        path = reverse('BuildRoad', kwargs={'pk': 1})
+        data = {"type": "build_road",
+                "payload": [{"level": 2, "index": 1},
+                            {"level": 2, "index": 2}]}
+        request = RequestFactory().post(path, data,
+                                        content_type='application/json')
+        force_authenticate(request, user=self.user, token=self.token)
+        view = BuildRoad.as_view()
+        response = view(request, pk=1)
+        response.render()
+        path_game = reverse('GameInfo', kwargs={'pk': 1})
+        request_game = RequestFactory().get(path_game)
+        force_authenticate(request_game, user=self.user, token=self.token)
+        view_game = GameInfo.as_view()
+        response_game = view_game(request_game, pk=1)
+        path_player = reverse('PlayerInfo', kwargs={'pk': 1})
+        request_player = RequestFactory().get(path_player)
+        force_authenticate(request_player, user=self.user, token=self.token)
+        view_player = PlayerInfo.as_view()
+        response_player = view_player(request_player, pk=1)
+        assert len(response_player.data['resources']) == 2
+        assert len(response_game.data['players'][0]['roads']) == 1
+        assert response.data == {"detail": "Not in turn"}
+        assert response.status_code == 403
 
     def test_not_neighbor(self):
         path = reverse('BuildRoad', kwargs={'pk': 1})
@@ -46,36 +90,6 @@ class TestViews(TestCase):
         assert response.data == {'detail': 'not neighbor'}
         assert response.status_code == 403
 
-    def test_not_resource(self):
-        path = reverse('BuildRoad', kwargs={'pk': 1})
-        data = {"type": "build_road",
-                "payload": [{"level": 2, "index": 0},
-                            {"level": 2, "index": 1}]}
-        request = RequestFactory().post(path, data,
-                                        content_type='application/json')
-        force_authenticate(request, user=self.user, token=self.token)
-        view = BuildRoad.as_view()
-        response = view(request, pk=1)
-        assert response.data == {'detail': "Doesn't have enough resources"}
-        assert response.status_code == 403
-
-    def test_nothing_built(self):
-        path = reverse('BuildRoad', kwargs={'pk': 1})
-        data = {"type": "build_road",
-                "payload": [{"level": 2, "index": 0},
-                            {"level": 2, "index": 1}]}
-        request = RequestFactory().post(path, data,
-                                        content_type='application/json')
-        force_authenticate(request, user=self.user, token=self.token)
-        Resource.objects.create(owner=self.player, game=self.game,
-                                resource_name='brick')
-        Resource.objects.create(owner=self.player, game=self.game,
-                                resource_name='lumber')
-        view = BuildRoad.as_view()
-        response = view(request, pk=1)
-        assert response.data == {'detail': 'must have something built'}
-        assert response.status_code == 403
-
     def test_invalid_position(self):
         path = reverse('BuildRoad', kwargs={'pk': 1})
         data = {"type": "build_road",
@@ -84,14 +98,22 @@ class TestViews(TestCase):
         request = RequestFactory().post(path, data,
                                         content_type='application/json')
         force_authenticate(request, user=self.user, token=self.token)
-        Resource.objects.create(owner=self.player, game=self.game,
-                                resource_name='brick')
-        Resource.objects.create(owner=self.player, game=self.game,
-                                resource_name='lumber')
-        Road.objects.create(owner=self.player, vertex_1=self.vert_position1,
-                            vertex_2=self.vert_position2, game=self.game)
+
         view = BuildRoad.as_view()
         response = view(request, pk=1)
+
+        path_game = reverse('GameInfo', kwargs={'pk': 1})
+        request_game = RequestFactory().get(path_game)
+        force_authenticate(request_game, user=self.user, token=self.token)
+        view_game = GameInfo.as_view()
+        response_game = view_game(request_game, pk=1)
+        path_player = reverse('PlayerInfo', kwargs={'pk': 1})
+        request_player = RequestFactory().get(path_player)
+        force_authenticate(request_player, user=self.user, token=self.token)
+        view_player = PlayerInfo.as_view()
+        response_player = view_player(request_player, pk=1)
+        assert len(response_player.data['resources']) == 2
+        assert len(response_game.data['players'][0]['roads']) == 1
         assert response.data == {'detail': 'invalid position, reserved'}
         assert response.status_code == 403
 
@@ -99,25 +121,115 @@ class TestViews(TestCase):
         self.token = AccessToken()
         path = reverse('BuildRoad', kwargs={'pk': 1})
         data = {"type": "build_road",
-                "payload": [{"level": 2, "index": 0},
-                            {"level": 2, "index": 1}]}
+                "payload": [{"level": 2, "index": 1},
+                            {"level": 2, "index": 2}]}
         request = RequestFactory().post(path, data,
                                         content_type='application/json')
         request.user = User.objects.create(username='nvero',
                                            password='bariloche')
         force_authenticate(request, user=self.user, token=self.token)
-        Resource.objects.create(owner=self.player, game=self.game,
-                                resource_name='brick')
-        Resource.objects.create(owner=self.player, game=self.game,
-                                resource_name='lumber')
-        vp2 = VertexPosition.objects.create(level=2, index=2)
-        road = Road.objects.create(owner=self.player,
-                                   vertex_1=self.vert_position2,
-                                   vertex_2=vp2,
-                                   game=self.game)
-        building = Building.objects.create(game=self.game, name='settlement',
-                                           owner=self.player,
-                                           position=self.vert_position1)
+
         view = BuildRoad.as_view()
         response = view(request, pk=1)
+        response.render()
+        path_game = reverse('GameInfo', kwargs={'pk': 1})
+        request_game = RequestFactory().get(path_game)
+        force_authenticate(request_game, user=self.user, token=self.token)
+        view_game = GameInfo.as_view()
+        response_game = view_game(request_game, pk=1)
+        path_player = reverse('PlayerInfo', kwargs={'pk': 1})
+        request_player = RequestFactory().get(path_player)
+        force_authenticate(request_player, user=self.user, token=self.token)
+        view_player = PlayerInfo.as_view()
+        response_player = view_player(request_player, pk=1)
+        assert len(response_player.data['resources']) == 0
+        assert response_game.data['players'][0]['roads'][0][0]['level'] == 2
+        assert response_game.data['players'][0]['roads'][0][0]['index'] == 0
+        assert response_game.data['players'][0]['roads'][0][1]['level'] == 2
+        assert response_game.data['players'][0]['roads'][0][1]['index'] == 1
+        assert response_game.data['players'][0]['roads'][1][0]['level'] == 2
+        assert response_game.data['players'][0]['roads'][1][0]['index'] == 1
+        assert response_game.data['players'][0]['roads'][1][1]['level'] == 2
+        assert response_game.data['players'][0]['roads'][1][1]['index'] == 2
+        print(response_game.data['players'][0]['roads'])
+        assert len(response_game.data['players'][0]['roads']) == 2
         assert response.status_code == 200
+
+    def test_nothing_built(self):
+        path = reverse('BuildRoad', kwargs={'pk': 1})
+        data = {"type": "build_road",
+                "payload": [{"level": 2, "index": 1},
+                            {"level": 2, "index": 2}]}
+        request = RequestFactory().post(path, data,
+                                        content_type='application/json')
+        force_authenticate(request, user=self.user, token=self.token)
+        self.road.delete()
+        view = BuildRoad.as_view()
+        response = view(request, pk=1)
+
+        path_game = reverse('GameInfo', kwargs={'pk': 1})
+        request_game = RequestFactory().get(path_game)
+        force_authenticate(request_game, user=self.user, token=self.token)
+        view_game = GameInfo.as_view()
+        response_game = view_game(request_game, pk=1)
+        path_player = reverse('PlayerInfo', kwargs={'pk': 1})
+        request_player = RequestFactory().get(path_player)
+        force_authenticate(request_player, user=self.user, token=self.token)
+        view_player = PlayerInfo.as_view()
+        response_player = view_player(request_player, pk=1)
+        assert len(response_player.data['resources']) == 2
+        assert response_game.data['players'][0]['roads'] == []
+        assert response.data == {'detail': 'must have something built'}
+        assert response.status_code == 403
+
+    def test_not_resource(self):
+        path = reverse('BuildRoad', kwargs={'pk': 1})
+        data = {"type": "build_road",
+                "payload": [{"level": 2, "index": 1},
+                            {"level": 2, "index": 2}]}
+        request = RequestFactory().post(path, data,
+                                        content_type='application/json')
+        force_authenticate(request, user=self.user, token=self.token)
+        self.brick.delete()
+        self.lumber.delete()
+        view = BuildRoad.as_view()
+        response = view(request, pk=1)
+        path_game = reverse('GameInfo', kwargs={'pk': 1})
+        request_game = RequestFactory().get(path_game)
+        force_authenticate(request_game, user=self.user, token=self.token)
+        view_game = GameInfo.as_view()
+        response_game = view_game(request_game, pk=1)
+        path_player = reverse('PlayerInfo', kwargs={'pk': 1})
+        request_player = RequestFactory().get(path_player)
+        force_authenticate(request_player, user=self.user, token=self.token)
+        view_player = PlayerInfo.as_view()
+        response_player = view_player(request_player, pk=1)
+        assert len(response_player.data['resources']) == 0
+        assert len(response_game.data['players'][0]['roads']) == 1
+        assert response.data == {'detail': "Doesn't have enough resources"}
+        assert response.status_code == 403
+
+    def test_not_exist_vertex(self):
+        path = reverse('BuildRoad', kwargs={'pk': 1})
+        data = {"type": "build_road",
+                "payload": [{"level": 200, "index": 10000},
+                            {"level": 2, "index": 2}]}
+        request = RequestFactory().post(path, data,
+                                        content_type='application/json')
+        force_authenticate(request, user=self.user, token=self.token)
+        view = BuildRoad.as_view()
+        response = view(request, pk=1)
+        path_game = reverse('GameInfo', kwargs={'pk': 1})
+        request_game = RequestFactory().get(path_game)
+        force_authenticate(request_game, user=self.user, token=self.token)
+        view_game = GameInfo.as_view()
+        response_game = view_game(request_game, pk=1)
+        path_player = reverse('PlayerInfo', kwargs={'pk': 1})
+        request_player = RequestFactory().get(path_player)
+        force_authenticate(request_player, user=self.user, token=self.token)
+        view_player = PlayerInfo.as_view()
+        response_player = view_player(request_player, pk=1)
+        assert len(response_player.data['resources']) == 2
+        assert len(response_game.data['players'][0]['roads']) == 1
+        assert response.data == {"detail": "Non-existent vetertexs positions"}
+        assert response.status_code == 403
--- backend/catan/tests/test_cargaJson.py
+++ backend/catan/tests/test_cargaJson.py
@@ -30,6 +30,23 @@ class TestView(TestCase):
                                             development_cards=0,
                                             resources_cards=2,
                                             victory_points=0)
+        self.brick = Resource.objects.create(owner=self.player, game=self.game,
+                                             resource_name='brick')
+        self.lumber = Resource.objects.create(owner=self.player,
+                                              game=self.game,
+                                              resource_name='lumber')
+        self.road = Road.objects.create(owner=self.player,
+                                        vertex_1=self.vert_pos1,
+                                        vertex_2=self.vert_pos2,
+                                        game=self.game)
+        self.building1 = Building.objects.create(game=self.game,
+                                                 name='settlement',
+                                                 owner=self.player,
+                                                 position=self.vert_pos1)
+        self.building2 = Building.objects.create(game=self.game,
+                                                 name='settlement',
+                                                 owner=self.player,
+                                                 position=self.vert_pos2)
 
     def test_is_neighbor(self):
         vp1 = VertexPosition.objects.create(level=2, index=18)
@@ -50,53 +67,40 @@ class TestView(TestCase):
         list_vec = VertexInfo(self.vert_pos1.level, self.vert_pos1.index)
         self.assertEqual(is_neighbor(list_vec, self.vert_pos2.level,
                                      self.vert_pos2.index), True)
-        Road.objects.create(owner=self.player, vertex_1=self.vert_pos1,
-                            vertex_2=self.vert_pos2, game=self.game)
-        list_road = Road.objects.filter(owner=self.player, game=self.game)
-        self.assertTrue(CheckRoads_Road(list_road, self.vert_pos1.level,
-                                        self.vert_pos1.index, 2, 29))
-        self.assertTrue(CheckRoads_Road(list_road, self.vert_pos2.level,
-                                        self.vert_pos2.index, 1, 1))
-        self.assertTrue(CheckRoads_Road(list_road, 1, 1, self.vert_pos2.level,
-                                        self.vert_pos2.index))
-        self.assertFalse(CheckRoads_Road(list_road, 1, 17, 1, 16))
+        assert CheckRoads_Road(self.player.id, self.game.id,
+                               self.vert_pos1.level,
+                               self.vert_pos1.index, 2, 29) is True
+        assert CheckRoads_Road(self.player.id, self.game.id,
+                               self.vert_pos2.level,
+                               self.vert_pos2.index, 1, 1) is True
+        assert CheckRoads_Road(self.player.id, self.game.id, 1, 1,
+                               self.vert_pos2.level,
+                               self.vert_pos2.index) is True
+        assert CheckRoads_Road(self.player.id, self.game.id, 1, 1,
+                               self.vert_pos1.level,
+                               self.vert_pos1.index) is True
+        assert CheckRoads_Road(self.player.id, self.game.id, 1, 17, 1,
+                               16) is False
 
     def test_CheckBuild_Road(self):
-        Building.objects.create(game=self.game, name='settlement',
-                                owner=self.player,
-                                position=self.vert_pos1)
-        Building.objects.create(game=self.game, name='settlement',
-                                owner=self.player,
-                                position=self.vert_pos2)
-        list_build = Building.objects.filter(owner=self.player, game=self.game)
-        self.assertEqual(CheckBuild_Road(list_build, self.vert_pos1.level,
-                                         self.vert_pos1.index, 1, 1), True)
-        self.assertEqual(CheckBuild_Road(list_build, 1, 1,
-                                         self.vert_pos2.level,
-                                         self.vert_pos2.index), True)
-        self.assertEqual(CheckBuild_Road(list_build, 1, 17, 1, 16), False)
+        assert CheckBuild_Road(self.player.id, self.game.id,
+                               self.vert_pos1.level,
+                               self.vert_pos1.index, 1, 1) is True
+        assert CheckBuild_Road(self.player.id, self.game.id, 1, 1,
+                               self.vert_pos2.level,
+                               self.vert_pos2.index) is True
+        assert CheckBuild_Road(self.player.id, self.game.id, 1, 17, 1,
+                               16) is False
 
     def test_CheckPositionRoad(self):
-        Road.objects.create(owner=self.player, vertex_1=self.vert_pos1,
-                            vertex_2=self.vert_pos2, game=self.game)
-        list_road = Road.objects.filter(owner=self.player, game=self.game)
-        self.assertTrue(CheckPositionRoad(list_road, self.vert_pos1.level,
-                        self.vert_pos1.index, self.vert_pos2.level,
-                        self.vert_pos2.index))
-        self.assertTrue(CheckPositionRoad(list_road, self.vert_pos2.level,
-                        self.vert_pos2.index, self.vert_pos1.level,
-                        self.vert_pos1.index))
-        self.assertEqual(CheckPositionRoad(list_road, self.vert_pos1.level,
-                                           self.vert_pos1.index, 1, 1), False)
+        assert CheckPositionRoad(self.game.id, self.vert_pos1.level,
+                                 self.vert_pos1.index, self.vert_pos2.level,
+                                 self.vert_pos2.index) is True
+        assert CheckPositionRoad(self.game.id, self.vert_pos2.level,
+                                 self.vert_pos2.index, self.vert_pos1.level,
+                                 self.vert_pos1.index) is True
+        assert CheckPositionRoad(self.game.id, self.vert_pos1.level,
+                                 self.vert_pos1.index, 1, 1) is False
 
     def test_deleteResource(self):
-        Resource.objects.create(owner=self.player, game=self.game,
-                                resource_name='brick')
-        Resource.objects.create(owner=self.player, game=self.game,
-                                resource_name='lumber')
-        list_resource = Resource.objects.filter(owner=self.player,
-                                                game=self.game)
-        Road.objects.create(owner=self.player, vertex_1=self.vert_pos1,
-                            vertex_2=self.vert_pos2,
-                            game=self.game)
-        self.assertEqual(deleteResource(list_resource), None)
+        assert deleteResource(self.player.id, self.game.id) is None
