42d6ddc4b857afd7a5bfc852e519b6a0c4a48623
Veroo <verito.cba933@gmail.com>
2019-11-03 20:55:44 -0300

    Cambio de nombre, en el archivo test_road_url.py

--- backend/catan/tests/test_road_url.py
+++ backend/catan/tests/test_road_url.py
@@ -3,6 +3,6 @@ from django.urls import reverse, resolve
 
 
 class TestUrls:
-    def test_BuildRoad_url(self):
-        path = reverse('BuildRoad', kwargs={'pk': 1})
-        assert resolve(path).view_name == 'BuildRoad'
+    def test_PlayerActions_url(self):
+        path = reverse('PlayerActions', kwargs={'pk': 1})
+        assert resolve(path).view_name == 'PlayerActions'
