9cc51d80b31fb1b001b341c068eb6d9c94ed38c8
Pablo Rosa <bluchisnico@gmail.com>
2019-11-03 15:57:41 -0300

    Modularizando funcion throw_dices

--- backend/catan/dices.py
+++ backend/catan/dices.py
@@ -1,5 +1,7 @@
 """
 Module to manage the throw of the dices in the game
+and the distribution of resources after the throw.
+Also has the logic to discart cards when the sum is equal to 7.
 """
 from catan.models import *
 from random import random
@@ -8,7 +10,7 @@ from catan.cargaJson import HexagonInfo
 
 def throw_dice():
     """
-    A method to generate a thow of dice
+    A function to generate a thow of dice
     (uniform discrete distribution).
     """
     return 1 * int(6 * random()) + 1
@@ -58,17 +60,32 @@ def gain_resources(game, owner, resource_name, amount):
     owner.save()
 
 
-def throw_dices(game):
-    # Throw the dices
-    dice1 = throw_dice()
-    dice2 = throw_dice()
-    # Get the sum of dices
-    sum_dices = dice1 + dice2
-    # Get the hexes with this token from the board
-    hexes = Hexe.objects.filter(board=game.board, token=sum_dices)
-    # Get the players of the games
-    players = Player.objects.filter(game=game)
-    set_players_resources_not_last_gained(players)
+def throw_twoDices(dice1=0, dice2=0):
+    """
+    A function to get the throw of two dices
+    Params:
+    @dice1: the value of the dice1 (used only for testing).
+    @dice2: the value of the dice2 (used only for testing).
+    """
+    if (dice1 == 0) and (dice2 == 0):
+        dice1 = throw_dice()
+        dice2 = throw_dice()
+        return (dice1, dice2)
+    else:
+        return (dice1, dice2)
+
+
+def distribute_resources(hexes, players, game):
+    """
+    A method that takes a list of hexagons and a list of players.
+    For each hex on the list, check if each player on the list has
+    buildings at the vertices of that hex; if the player has them
+    increases his amount of resources.
+    Params:
+    @hexes: a list of selected objects Hexes of the board.
+    @players: a list of players of a started game.
+    @game: a started game.
+    """
     if len(hexes) != 0:
         for hexe in hexes:
             hexe_level = hexe.position.level
@@ -90,6 +107,31 @@ def throw_dices(game):
                                 gain_resources(owner=player, game=game,
                                                resource_name=hexe.terrain,
                                                amount=2)
-    game.current_turn.dices1 = dice1
-    game.current_turn.dices2 = dice2
+
+
+def throw_dices(game, dice1=0, dice2=0):
+    """
+    A method that rolls the two dice at the beginning of the turn and
+    distributes the resources according to the hexagons with the
+    token of the sum of the dice.
+    Params:
+    @game: a started game.
+    @dice1: the value of the dice1 (used only for testing).
+    @dice2: the value of the dice2 (used only for testing).
+    """
+    # Get the trow of dices and then sum them.
+    two_dices = throw_twoDices(dice1, dice2)
+    sum_dices = sum(two_dices)
+    # Get the players of the games
+    players = Player.objects.filter(game=game)
+    # if sum_dices == 7:
+    #   random_dicart(players)
+    # else (poner lo que sigue en else hasta inclusive distribute_resources):
+    set_players_resources_not_last_gained(players)
+    # Get the hexes with this token from the board
+    hexes = Hexe.objects.filter(board=game.board, token=sum_dices)
+    # Then distribute_resources...
+    distribute_resources(hexes, players, game)
+    game.current_turn.dices1 = two_dices[0]
+    game.current_turn.dices2 = two_dices[1]
     game.current_turn.save()
