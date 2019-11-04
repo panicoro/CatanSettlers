60c46cf08c06a11002a52932128dd9e63710ec2d
Naigaribay <nairagaribay@gmail.com>
2019-11-04 11:28:07 -0300

    Organizando tests

--- backend/catan/tests/test_register_url.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from django.urls import reverse, resolve
-
-
-class TestUrls:
-    def test_Register_url(self):
-        path = reverse('register')
-        assert resolve(path).view_name == 'register'
