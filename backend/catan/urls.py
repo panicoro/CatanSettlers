cb63401f8ea434a3ce82a4c5688f0d2fb9ecfb81
Pablo Rosa <bluchisnico@gmail.com>
2019-11-04 18:15:44 -0300

    Corriegiendo llamado de vistas desde nueva ubicacion en url.py

--- backend/catan/urls.py
+++ backend/catan/urls.py
@@ -1,27 +1,30 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_simplejwt.views import TokenRefreshView
-from catan import views
+from catan.views import *
 
 urlpatterns = [
-    path('rooms/', views.RoomList.as_view(), name='list_rooms'),
-    path('rooms/<int:pk>/', views.RoomDetail.as_view(), name='join_room'),
-    path('users/login/', views.AuthAPIView.as_view(),
+    # Rooms views
+    path('rooms/', room_views.RoomList.as_view(), name='list_rooms'),
+    path('rooms/<int:pk>/', room_views.RoomDetail.as_view(), name='join_room'),
+    # Login/Register views
+    path('users/login/', login_views.AuthAPIView.as_view(),
          name='tokenObtainPair'),
-    path('users/login/refresh/', TokenRefreshView.as_view(),
+    path('users/login/refresh/', login_views.TokenRefreshView.as_view(),
          name='refreshToken'),
-    path('users/', views.Register.as_view(),
+    path('users/', login_views.Register.as_view(),
          name='register'),
-    path('games/<int:pk>/player', views.PlayerInfo.as_view(),
-         name='PlayerInfo'),
-    path('games/<int:pk>/', views.GameInfo.as_view(),
-         name='GameInfo'),
-    path('games/', views.GameList.as_view(), name='Games'),
-    path('boards/', views.BoardList.as_view(), name='Boards'),
-    path('games/<int:pk>/board/', views.BoardInfo.as_view(),
-         name='BoardInfo'),
-    path('games/<int:pk>/player/actions', views.PlayerActions.as_view(),
+    # Players views
+    path('games/<int:pk>/player/actions', players_views.PlayerActions.as_view(),
          name='PlayerActions'),
+    path('games/<int:pk>/player', players_views.PlayerInfo.as_view(),
+         name='PlayerInfo'),
+    path('games/<int:pk>/', game_views.GameInfo.as_view(),
+         name='GameInfo'),
+    path('games/', game_views.GameList.as_view(), name='Games'),
+    path('boards/', board_views.BoardList.as_view(), name='Boards'),
+    path('games/<int:pk>/board/', board_views.BoardInfo.as_view(),
+         name='BoardInfo'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
