fb076bb41d412a78854533576b01a0961058891e
Pablo Rosa <bluchisnico@gmail.com>
2019-11-06 17:18:23 -0300

    Agregando correcion en dices.py en la funcion gain_resources

--- backend/catan/dices.py
+++ backend/catan/dices.py
@@ -56,8 +56,6 @@ def gain_resources(game, owner, resource_name, amount):
         Resource.objects.create(owner=owner, game=game,
                                 resource_name=resource_name,
                                 last_gained=True)
-    owner.resources_cards += amount
-    owner.save()
 
 
 def throw_twoDices(dice1=0, dice2=0):
