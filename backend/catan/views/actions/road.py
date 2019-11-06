20c1295c8942a9f9bccdfc568d165f92eac2b864
Naigaribay <nairagaribay@gmail.com>
2019-11-06 16:28:47 -0300

    Agregando restriciones

--- backend/catan/views/actions/road.py
+++ backend/catan/views/actions/road.py
@@ -132,6 +132,22 @@ def create_Road(game, player, level1, index1, level2, index2):
     new_road.save()
 
 
+def checkCard(game_id, player_id):
+    rta = False
+    card = Card.objects.filter(owner=player_id, game=game_id,
+                               card_name='road_building')[:1]
+    if card.exists():
+        rta = True
+        return rta
+    return rta
+
+
+def deleteCard(game_id, player_id):
+    card = Card.objects.filter(owner=player_id, game=game_id,
+                               card_name='road_building')
+    card.delete()
+
+
 def build_road(payload, game, owner):
     level1 = payload[0]['level']
     index1 = payload[0]['index']
@@ -171,15 +187,54 @@ def build_road(payload, game, owner):
 
 
 def play_road_building_card(payload, game, player):
-    response = build_road(payload, game, player)
-    print("<xsaxaxasx")
-    print(response)
-    if response.data == {"detail": "Doesn't have enough resources"}:
-        level1 = payload[0]['level']
-        index1 = payload[0]['index']
-        level2 = payload[1]['level']
-        index2 = payload[1]['index']
-        create_Road(game, player, level1, index1, level2, index2)
-        deleteResource(player.id, game.id)
-        return Response(status=status.HTTP_200_OK)
-    return Response(response.data, status=status.HTTP_403_FORBIDDEN)
+    position_1 = payload[0]
+    position_2 = payload[1]
+    level1 = position_1[0]['level']
+    index1 = position_1[0]['index']
+    level2 = position_1[1]['level']
+    index2 = position_1[1]['index']
+    level3 = position_2[0]['level']
+    index3 = position_2[0]['index']
+    level4 = position_2[1]['level']
+    index4 = position_2[1]['index']
+    # Check the player owns the card
+    if not checkCard(game.id, player.id):
+        response = {"detail": "Does not have the letter"}
+        return Response(response, status=status.HTTP_403_FORBIDDEN)
+    # Check that the position exists
+    if (not checkVertexsPositions(level1, index1, level2, index2) or
+            not checkVertexsPositions(level3, index3, level4, index4)):
+        response = {"detail": "Non-existent vetertexs positions"}
+        return Response(response, status=status.HTTP_403_FORBIDDEN)
+    listNeighbor_road1 = VertexInfo(level1, index1)
+    listNeighbor_road2 = VertexInfo(level3, index3)
+    # check that the neighbor exists
+    if (not is_neighbor(listNeighbor_road1, level2, index2) or
+            not is_neighbor(listNeighbor_road2, level4, index4)):
+        response = {"detail": "not neighbor"}
+        return Response(response, status=status.HTTP_403_FORBIDDEN)
+    position_road1 = CheckPositionRoad(game.id, level1, index1,
+                                       level2, index2)
+    position_road2 = CheckPositionRoad(game.id, level3, index3,
+                                       level3, index3)
+    # I check if the position is free
+    if position_road1 or position_road2:
+        response = {"detail": "invalid position, reserved"}
+        return Response(response, status=status.HTTP_403_FORBIDDEN)
+    isRoads_road1 = CheckRoads_Road(player.id, game.id, level1, index1,
+                                    level2, index2)
+    isBuilding_road1 = CheckBuild_Road(player.id, game.id, level1, index1,
+                                       level2, index2)
+    isRoads_road2 = CheckRoads_Road(player.id, game.id, level1, index1,
+                                    level2, index2)
+    isBuilding_road2 = CheckBuild_Road(player.id, game.id, level1, index1,
+                                       level2, index2)
+    # I verify that I have my own road or building
+    if (not isRoads_road1 and not isBuilding_road1 and
+            not isRoads_road2 and not isBuilding_road2):
+        response = {"detail": "must have something built"}
+        return Response(response, status=status.HTTP_403_FORBIDDEN)
+    create_Road(game, player, level1, index1, level2, index2)
+    create_Road(game, player, level3, index3, level4, index4)
+    deleteCard(game.id, player.id)
+    return Response(status=status.HTTP_200_OK)
