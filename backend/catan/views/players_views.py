442c4b79db849df5803fab7ca4571c3cb7b5cd91
Pablo Rosa <bluchisnico@gmail.com>
2019-11-06 05:24:53 -0300

    Agregando funciones para implementar el get de build_settlements

--- backend/catan/views/players_views.py
+++ backend/catan/views/players_views.py
@@ -1,27 +1,15 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
-from rest_framework import generics, permissions, status
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from rest_framework_simplejwt import authentication
 from catan.serializers import *
-from catan.dices import throw_dices
 from django.http import Http404
-from random import random, randint
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from catan.models import *
-from catan.cargaJson import *
-from catan.dices import throw_dices
-from rest_framework.permissions import AllowAny
-from random import shuffle
 from catan.views.actions.road import build_road
-from catan.views.actions.build import build_settlement
+from catan.views.actions.build import (
+            build_settlement, canBuild_Settlement,
+            posiblesSettlements)
 from catan.views.actions.robber import move_robber
 from catan.views.actions.play_cards import move_robberCard
 
@@ -47,6 +35,7 @@ class PlayerInfo(APIView):
 
 
 class PlayerActions(APIView):
+
     def check_player_in_turn(self, game, player):
         """
         A method to check if the player is in turn in the given game.
@@ -56,22 +45,27 @@ class PlayerActions(APIView):
         """
         return game.current_turn.user == player.username
 
-    """def Road(self, game, player, level1, index1, level2, index2):
-        position_1 = VertexPosition.objects.filter(level=level1,
-                                                   index=index1).get()
-        position_2 = VertexPosition.objects.filter(level=level2,
-                                                   index=index2).get()
-        new_road = Road(game=game, vertex_1=position_1,
-                        vertex_2=position_2, owner=player)
-        new_road.save()
-    """
+    def get(self, request, pk):
+        game = get_object_or_404(Game, pk=pk)
+        user = request.user
+        player = get_object_or_404(Player, username=user, game=game)
+        data = []
+        if canBuild_Settlement(player):
+            item = {"type": 'build_settlement'}
+            posibles_setlements = posiblesSettlements(player)
+            serialized_positions = VertexPositionSerializer(
+                                        posibles_setlements,
+                                        many=True)
+            item['payload'] = serialized_positions.data
+            if len(item['payload']) != 0:
+                data.append(item)
+        return Response(data, status=status.HTTP_200_OK)
 
     def post(self, request, pk):
         data = request.data
         game = get_object_or_404(Game, pk=pk)
-        player = get_object_or_404(Player, username=request.user, game=game)
-        my_user = request.user
-        my_player = Player.objects.get(username=my_user, game=game)
+        user = request.user
+        player = get_object_or_404(Player, username=user, game=game)
         # Check if the player is on his turn
         if not self.check_player_in_turn(game, player):
             response = {"detail": "Not in turn"}
@@ -79,19 +73,14 @@ class PlayerActions(APIView):
         if data['type'] == 'build_settlement':
             response = build_settlement(data['payload'], game, player)
             return response
-        user = self.request.user
-        owner = Player.objects.filter(username=user, game=pk).get()
         if data['type'] == 'build_road':
-            response = build_road(data['payload'], game, owner)
+            response = build_road(data['payload'], game, player)
             return response
-
         if data['type'] == 'move_robber':
-            response = move_robber(data['payload'], game, my_user, my_player)
+            response = move_robber(data['payload'], game, user, player)
             return response
-
         if data['type'] == 'play_knight_card':
-            response = move_robberCard(data['payload'], game, my_user, my_player)
+            response = move_robberCard(data['payload'], game, user, player)
             return response
-
         response = {"detail": 'Please select a valid action'}
         return Response(response, status=status.HTTP_403_FORBIDDEN)
