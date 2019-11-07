1fa0fc0e4340b8a7ebeaa763c1b7c16e57f71993
Naigaribay <nairagaribay@gmail.com>
2019-11-06 23:07:50 -0300

    Agregando test de las demas resticciones

--- backend/catan/tests/cards/test_cards_roadBuild.py
+++ backend/catan/tests/cards/test_cards_roadBuild.py
@@ -22,6 +22,7 @@ class TestViews(TestCase):
         self.vert_position2 = VertexPosition.objects.create(level=2, index=1)
         self.vert_position3 = VertexPosition.objects.create(level=2, index=2)
         self.vert_position4 = VertexPosition.objects.create(level=2, index=3)
+        self.vert_position4 = VertexPosition.objects.create(level=2, index=5)
         self.hexe_position = HexePosition.objects.create(level=0, index=0)
         self.board = Board.objects.create(name='Colonos')
         self.game = Game.objects.create(id=1, name='Juego1', board=self.board,
@@ -34,18 +35,12 @@ class TestViews(TestCase):
                                                 user=self.user,
                                                 dices1=3,
                                                 dices2=3)
-        self.brick = Resource.objects.create(owner=self.player,
-                                             game=self.game,
-                                             resource_name="brick")
-        self.lumber = Resource.objects.create(owner=self.player,
-                                              game=self.game,
-                                              resource_name="lumber")
         self.road = Road.objects.create(owner=self.player,
                                         vertex_1=self.vert_position1,
                                         vertex_2=self.vert_position2,
                                         game=self.game)
-        self.cardRoad = Card.objects.create(owner=self.player, game=self.game, card_name='road_building')
-
+        self.cardRoad = Card.objects.create(owner=self.player, game=self.game,
+                                            card_name='road_building')
 
     def test_roadBuilding_card(self):
         path = reverse('PlayerActions', kwargs={'pk': 1})
@@ -65,14 +60,160 @@ class TestViews(TestCase):
         view_game = GameInfo.as_view()
         response_game = view_game(request_game, pk=1)
         response_game.render()
-        print(response_game.data['players'][0]['roads'])
         path_player = reverse('PlayerInfo', kwargs={'pk': 1})
         request_player = RequestFactory().get(path_player)
         force_authenticate(request_player, user=self.user, token=self.token)
         view_player = PlayerInfo.as_view()
         response_player = view_player(request_player, pk=1)
-        print(response_player.data['resources'])
-        assert len(response_player.data['resources']) == 2
-        assert len(response_game.data['players'][0]['roads'][0]) == 2
-        print(response.data)
+        print(response_player.data)
+        assert len(response_player.data['cards']) == 0
+        assert len(response_game.data['players'][0]['roads']) == 3
+        assert response_game.data['players'][0]['development_cards'] == 0
         assert response.status_code == 200
+
+    def test_noCard(self):
+        self.cardRoad.delete()
+        path = reverse('PlayerActions', kwargs={'pk': 1})
+        data = {"type": "play_road_building_card",
+                "payload": [[{"level": 2, "index": 1},
+                            {"level": 2, "index": 2}],
+                            [{"level": 2, "index": 2},
+                            {"level": 2, "index": 3}]]}
+        request = RequestFactory().post(path, data,
+                                        content_type='application/json')
+        force_authenticate(request, user=self.user, token=self.token)
+        view = PlayerActions.as_view()
+        response = view(request, pk=1)
+        path_game = reverse('GameInfo', kwargs={'pk': 1})
+        request_game = RequestFactory().get(path_game)
+        force_authenticate(request_game, user=self.user, token=self.token)
+        view_game = GameInfo.as_view()
+        response_game = view_game(request_game, pk=1)
+        response_game.render()
+        path_player = reverse('PlayerInfo', kwargs={'pk': 1})
+        request_player = RequestFactory().get(path_player)
+        force_authenticate(request_player, user=self.user, token=self.token)
+        view_player = PlayerInfo.as_view()
+        response_player = view_player(request_player, pk=1)
+        assert len(response_player.data['cards']) == 0
+        assert len(response_game.data['players'][0]['roads']) == 1
+        assert response_game.data['players'][0]['development_cards'] == 0
+        assert response.data == {'detail': 'Does not have the letter'}
+        assert response.status_code == 403
+
+    def test_noVertex(self):
+        path = reverse('PlayerActions', kwargs={'pk': 1})
+        data = {"type": "play_road_building_card",
+                "payload": [[{"level": 200, "index": 1},
+                            {"level": 2, "index": 2}],
+                            [{"level": 2, "index": 2000},
+                            {"level": 2, "index": 3}]]}
+        request = RequestFactory().post(path, data,
+                                        content_type='application/json')
+        force_authenticate(request, user=self.user, token=self.token)
+        view = PlayerActions.as_view()
+        response = view(request, pk=1)
+        path_game = reverse('GameInfo', kwargs={'pk': 1})
+        request_game = RequestFactory().get(path_game)
+        force_authenticate(request_game, user=self.user, token=self.token)
+        view_game = GameInfo.as_view()
+        response_game = view_game(request_game, pk=1)
+        response_game.render()
+        path_player = reverse('PlayerInfo', kwargs={'pk': 1})
+        request_player = RequestFactory().get(path_player)
+        force_authenticate(request_player, user=self.user, token=self.token)
+        view_player = PlayerInfo.as_view()
+        response_player = view_player(request_player, pk=1)
+        assert len(response_player.data['cards']) == 1
+        assert len(response_game.data['players'][0]['roads']) == 1
+        assert response_game.data['players'][0]['development_cards'] == 1
+        assert response.data == {'detail': 'Non-existent vetertexs positions'}
+        assert response.status_code == 403
+
+    def test_busyPosition(self):
+        path = reverse('PlayerActions', kwargs={'pk': 1})
+        data = {"type": "play_road_building_card",
+                "payload": [[{"level": 2, "index": 0},
+                            {"level": 2, "index": 1}],
+                            [{"level": 2, "index": 2},
+                            {"level": 2, "index": 3}]]}
+        request = RequestFactory().post(path, data,
+                                        content_type='application/json')
+        force_authenticate(request, user=self.user, token=self.token)
+        view = PlayerActions.as_view()
+        response = view(request, pk=1)
+        path_game = reverse('GameInfo', kwargs={'pk': 1})
+        request_game = RequestFactory().get(path_game)
+        force_authenticate(request_game, user=self.user, token=self.token)
+        view_game = GameInfo.as_view()
+        response_game = view_game(request_game, pk=1)
+        response_game.render()
+        path_player = reverse('PlayerInfo', kwargs={'pk': 1})
+        request_player = RequestFactory().get(path_player)
+        force_authenticate(request_player, user=self.user, token=self.token)
+        view_player = PlayerInfo.as_view()
+        response_player = view_player(request_player, pk=1)
+        assert len(response_player.data['cards']) == 1
+        assert len(response_game.data['players'][0]['roads']) == 1
+        assert response_game.data['players'][0]['development_cards'] == 1
+        assert response.data == {'detail': 'invalid position, reserved'}
+        assert response.status_code == 403
+
+    def test_noRoad(self):
+        self.road.delete()
+        path = reverse('PlayerActions', kwargs={'pk': 1})
+        data = {"type": "play_road_building_card",
+                "payload": [[{"level": 2, "index": 1},
+                            {"level": 2, "index": 2}],
+                            [{"level": 2, "index": 2},
+                            {"level": 2, "index": 3}]]}
+        request = RequestFactory().post(path, data,
+                                        content_type='application/json')
+        force_authenticate(request, user=self.user, token=self.token)
+        view = PlayerActions.as_view()
+        response = view(request, pk=1)
+        path_game = reverse('GameInfo', kwargs={'pk': 1})
+        request_game = RequestFactory().get(path_game)
+        force_authenticate(request_game, user=self.user, token=self.token)
+        view_game = GameInfo.as_view()
+        response_game = view_game(request_game, pk=1)
+        response_game.render()
+        path_player = reverse('PlayerInfo', kwargs={'pk': 1})
+        request_player = RequestFactory().get(path_player)
+        force_authenticate(request_player, user=self.user, token=self.token)
+        view_player = PlayerInfo.as_view()
+        response_player = view_player(request_player, pk=1)
+        assert len(response_player.data['cards']) == 1
+        assert len(response_game.data['players'][0]['roads']) == 0
+        assert response_game.data['players'][0]['development_cards'] == 1
+        assert response.data == {'detail': 'must have something built'}
+        assert response.status_code == 403
+
+    def test_noNeighbor(self):
+        path = reverse('PlayerActions', kwargs={'pk': 1})
+        data = {"type": "play_road_building_card",
+                "payload": [[{"level": 2, "index": 5},
+                            {"level": 2, "index": 2}],
+                            [{"level": 2, "index": 2},
+                            {"level": 2, "index": 3}]]}
+        request = RequestFactory().post(path, data,
+                                        content_type='application/json')
+        force_authenticate(request, user=self.user, token=self.token)
+        view = PlayerActions.as_view()
+        response = view(request, pk=1)
+        path_game = reverse('GameInfo', kwargs={'pk': 1})
+        request_game = RequestFactory().get(path_game)
+        force_authenticate(request_game, user=self.user, token=self.token)
+        view_game = GameInfo.as_view()
+        response_game = view_game(request_game, pk=1)
+        response_game.render()
+        path_player = reverse('PlayerInfo', kwargs={'pk': 1})
+        request_player = RequestFactory().get(path_player)
+        force_authenticate(request_player, user=self.user, token=self.token)
+        view_player = PlayerInfo.as_view()
+        response_player = view_player(request_player, pk=1)
+        assert len(response_player.data['cards']) == 1
+        assert len(response_game.data['players'][0]['roads']) == 1
+        assert response_game.data['players'][0]['development_cards'] == 1
+        assert response.data == {'detail': 'not neighbor'}
+        assert response.status_code == 403
