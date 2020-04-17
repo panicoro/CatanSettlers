38a6cdbf3ebd73fb3c858e21a124006d855119c6
panicoro <panicoro770@gmail.com>
2020-04-17 19:43:29 -0300

    Adapting generateBoard without models HexePositions and VertexPosition

--- backend/aux/generateBoard.py
+++ backend/aux/generateBoard.py
@@ -1,8 +1,7 @@
 """
 Random Board Generator
 """
-from catan.models import (Hexe, HexePosition, Board,
-                          VertexPosition)
+from catan.models import Hexe, Board
 from random import randint, choice
 
 TYPE_RESOURCE = ['brick', 'wool', 'grain', 'ore', 'lumber']
@@ -11,13 +10,13 @@ TYPE_RESOURCE = ['brick', 'wool', 'grain', 'ore', 'lumber']
 def generateHexesPositions():
     """
     A method to generate all the positions of the hexagons in the board:
-    Generate only one time.
     """
+    positions = []
     top_ranges = [1, 6, 12]
     for i in range(0, 3):
         for j in range(0, top_ranges[i]):
-            new_hexe_position = HexePosition(level=i, index=j)
-            new_hexe_position.save()
+            positions.append((i, j))            
+    return positions
 
 
 def generateVertexPositions():
@@ -25,12 +24,12 @@ def generateVertexPositions():
     A method to generate all the positions of the vertex in the board:
     Generate only one time.
     """
+    positions = []
     top_ranges = [6, 18, 30]
     for i in range(0, 3):
         for j in range(0, top_ranges[i]):
-            new_vertex_position = VertexPosition(level=i, index=j)
-            new_vertex_position.save()
-
+            positions.append((i, j))
+    return positions
 
 def generateBoard(name):
     """
@@ -40,19 +39,22 @@ def generateBoard(name):
     """
     new_board = Board(name=name)
     new_board.save()
-    hexes_positions = HexePosition.objects.all()
+    hexes_positions = generateHexesPositions()
     # Choise one hexe_position for desert...
     position_for_desert = randint(0, 18)
     hexe_position_desert = hexes_positions[position_for_desert]
-    hexes_positions = hexes_positions.exclude(id=(position_for_desert + 1))
+    hexes_positions.remove(hexe_position_desert)
     hexe_desert = Hexe(board=new_board, terrain='desert',
-                       position=hexe_position_desert)
+                       level=hexe_position_desert[0], 
+                       index=hexe_position_desert[1])
     hexe_desert.save()
     for i in range(0, len(hexes_positions)):
         new_terrain = TYPE_RESOURCE[randint(0, 4)]
         new_token = choice([i for i in range(2, 12) if i not in [7]])
         new_hexe = Hexe(board=new_board, token=new_token,
-                        terrain=new_terrain, position=hexes_positions[i])
+                        terrain=new_terrain,
+                        level=hexes_positions[i][0],
+                        index=hexes_positions[i][1])
         new_hexe.save()
     return new_board
 
@@ -74,6 +76,7 @@ def generateBoardTest():
     11 => 'ore'
     12 => 'lumber'
     """
+    """
     new_board = Board(name="test_board")
     new_board.save()
     hexes_positions = HexePosition.objects.all()[:10]
@@ -86,3 +89,4 @@ def generateBoardTest():
                         terrain=new_terrain, position=hexes_positions[i])
         new_hexe.save()
     return new_board
+"""
