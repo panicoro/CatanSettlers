7ba0ce69d9dacb4ac5421fa39f51d18261edfc21
panicoro <panicoro770@gmail.com>
2020-05-21 15:30:41 -0300

    Adding test for road model

--- backend/catan/tests/road/test_road_model.py
+++ backend/catan/tests/road/test_road_model.py
@@ -60,3 +60,23 @@ class TestModels:
         except ValidationError as e:
             error = 'The index with level 2 must be between 0 and 29.'
             assert error in e.message_dict['__all__']
+
+    def test_road_player(self):
+        user_1 = mixer.blend(User, username='user1', password='hola1234')
+        board = mixer.blend('catan.Board', name='board_1')
+        hexe = mixer.blend('catan.Hexe', token=2, terrain='desert',
+                           board=board)
+        game1 = mixer.blend('catan.Game', name="Game1", board=board,
+                            robber=hexe)
+        game2 = mixer.blend('catan.Game', name="Game2", board=board,
+                            robber=hexe)
+        player1 = Player.objects.create(turn=1, username=user_1,
+                                        game=game2, colour="blue")
+        road = Road.objects.create(game=game1, owner=player1,
+                                   level_1=1, index_1=3,
+                                   level_2=0, index_2=1)
+        try:
+            road.full_clean()
+        except ValidationError as e:
+            error = 'Cannot be player of other game'
+            assert error in e.message_dict['__all__']
