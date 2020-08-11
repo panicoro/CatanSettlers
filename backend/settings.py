56c97b96d77d3a9e6c92868097522634f82dc0cc
panicoro <panicoro770@gmail.com>
2020-08-11 02:32:04 -0300

    Setting DEBUG to False

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2#4$=n1!!ce#7&lub%$)r^1thqujv!!8=l9l&o#ctw6ap++wz!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["settlerscatan.herokuapp.com"]
 
